02b3cdce9b8eb964b6a99bd448a53e3c557b8def
KrivovDE <55reigen55@mail.ru>
2022-11-04 16:05:38 +0300

    js module 2 lesson arrays

--- Tutorials for JS/module_2_OOP/module_2_lesson_1_array.js
+++ Tutorials for JS/module_2_OOP/module_2_lesson_1_array.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let ar1 = [];  //пустой массив
-let ar2 = [1, 30, 0, -2]; //одномерный массив чисел
-let ar3 = ["Яблоко", "Апельсин", "Слива"];
+// let ar1 = [];  //пустой массив
+// let ar2 = [1, 30, 0, -2]; //одномерный массив чисел
+// let ar3 = ["Яблоко", "Апельсин", "Слива"];
 
 // console.log( ar2[0] );
 // console.log( ar3[0] );
@@ -12,5 +12,78 @@ let ar3 = ["Яблоко", "Апельсин", "Слива"];
 // console.log( ar3[3] );
 
 
-for(let i=0;i < ar.length;++i)
-    console.log( ar[i] );
+// for(let i=0;i < ar3.length;++i)
+//     console.log( ar3[i] );
+
+// // Но можно это сделать и так:
+// console.log( ar3 );
+
+//
+// let ar = [ 'Яблоко', { name: 'Джон' }, true,
+//             function() { alert('привет'); } ];
+//
+// console.log( ar[1].name );
+
+// let digits = [];
+// digits[99999] = 1;
+// console.log(digits)
+
+// let fruits = ["Яблоко", "Апельсин", "Груша"];
+// for(let i=0;i < fruits.length; ++i)
+//     console.log( fruits[i] );
+// console.log('------------------------------')
+// for(let i of fruits)
+//     console.log(i);
+
+// let price = [2, 4, 6, 8, 10];
+// for(let i = 0; i < price.length; ++i)
+//     console.log(price[i] = price[i]**2);
+
+
+// Методы push/pop, shift/unshift и многомерные массивы
+//
+// let ar = [ "Яблоко", "Слива" ];
+// // for(let i of ar)
+// //     console.log(i);
+//
+// // ar[2] = "Груша";
+//
+// ar.push("Груша");
+// ar.pop()
+
+
+// Метод shift удаляет первый элемент массива:
+// метод unshift добавляет элемент в начало массива:
+
+
+
+// Многомерные массивы
+
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+// console.log( matrix );
+// console.log( matrix[2][1] );
+
+// for(let row of matrix) {
+//     let cols = "";
+//     for(let val of row)
+//        cols += val + " ";
+//
+//     console.log(cols);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Tutorials for JS/module_2_OOP/module_2_lesson_1_string.js
@@ -0,0 +1,54 @@
+// let str1 = "Hello World!";
+// let str2 = 'Hello World!';
+// let str3 = `Hello World!`;
+//
+// let name = "Java", age = 18;
+// let str = `Меня зовут ${name}, мне ${age} лет`;
+// console.log( str );
+
+//
+// •	\n – спецсимвол перевода на новую строку;
+// •	\r – возврат каретки (используется в ОС Windows совместно с символом \n);
+// •	\t – спецсимвол табуляции;
+// •	\uXXXX – символ в кодировке UTF-16;
+// •	\" и \' – символы кавычек (внутри строки);
+// •	\\ - символ обратного слеша.
+//
+// let str = 'Hello!\nI\'m Javascript.\nВот мой символ \t табуляции, обратный слеш \\ и символ \u00A9 копирайта';
+//
+// console.log( str );
+
+
+// let str = 'Hello!\nI\'m Javascript.';
+// console.log( str.length );
+//
+// let ch1 = str[0];
+// let ch2 = str[7];
+// console.log(ch1, ch2);
+// console.log(typeof ch1);
+
+let str = 'Hello!';
+let low = str.toLowerCase();
+let hi = "string".toUpperCase();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

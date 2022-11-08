7d4a97cac1553c22ef508bdf633589656d979a05
KrivovDE <55reigen55@mail.ru>
2022-11-08 18:34:38 +0300

    js module 2 lesson 2 __proto__

---
+++ Tutorials for JS/module_2_OOP/module_2_lesson_2/module_2_lesson_2_proto.js
@@ -0,0 +1,95 @@
+"use strict";
+
+// let geom = {
+//     name: "фигура",
+//     sp: {x: 0, y: 0},
+//     ep: {x: 100, y: 20}
+// };
+//
+// for(let key in geom) {
+//          console.log(key+": "+geom[key]);
+// }
+//
+// let rect = {
+//          draw() {
+//                    console.log("Рисование прямоугольника: " +
+//                             this.sp.x+","+this.sp.y+","+this.ep.x+","+this.ep.y);
+//          }
+// };
+//
+// rect.__proto__ = geom;
+//
+// for(let key in rect) {
+//          console.log(key+": "+rect[key]);
+// }
+// rect.draw()
+
+// let info = {
+//          getInfo() {
+//                    console.log(this.name);
+//          },
+//          __proto__: rect
+// }
+//
+// info.getInfo()
+//////////////////////////////////////////////////////////////////////
+// •	set <имя метода>([параметры]) {…} – свойство для записи значений в объект;
+// •	get <имя метода>([параметры]) {…} – свойство для чтения значений из объекта.
+
+
+// let geom = {
+//          name: "фигура",
+//          sp: {x: 0, y: 0},
+//          ep: {x: 100, y: 20},
+//          get nameGeom() {return this.name; },
+//          set nameGeom(name) {this.name = name; },
+// };
+// let rect = {
+//          draw() {
+//                    console.log("Рисование прямоугольника: " +
+//                             this.sp.x+","+this.sp.y+","+this.ep.x+","+this.ep.y);
+//          }
+// };
+//
+// rect.__proto__ = geom;
+//
+//
+// console.log(rect.nameGeom);      // чтение свойства
+// rect.nameGeom = "Прямоугольник"; // запись свойства
+// console.log(rect.nameGeom);
+// // console.log(geom.nameGeom);
+//////////////////////////////////////////////////////////////////////
+// // let prop = {
+// //          sp: {x: 0, y: 0},
+// //          ep: {x: 100, y: 20},
+// //          get coords() {
+// //                    return [this.sp.x, this.sp.y, this.ep.x, this.ep.y];
+// //          },
+// //          set coords(coords) {
+// //                    this.sp.x = coords[0]; this.sp.y = coords[1];
+// //                    this.ep.x = coords[2]; this.ep.y = coords[3];
+// //          }
+// // };
+//
+//
+// function Rect() {
+//          this.name = "прямоугольник";
+//
+//          this.draw = function() {
+//                    console.log("Рисование фигуры: "+this.name);
+//          }
+//          // this.__proto__ = prop;
+// }
+//   //this.prototype = prop;
+// let r = new Rect();
+// // r.draw();
+// // for(let p in r)
+// //          console.log(p+': '+r[p]);
+//
+
+
+
+
+
+
+

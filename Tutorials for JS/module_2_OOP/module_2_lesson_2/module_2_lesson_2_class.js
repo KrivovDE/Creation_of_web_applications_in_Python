17d61abc7aa891eb93ebc523dd1c8d37cbf6f1ea
KrivovDE <55reigen55@mail.ru>
2022-11-09 10:23:05 +0300

    js module 2 lesson 2 class

--- Tutorials for JS/module_2_OOP/module_2_lesson_2/module_2_lesson_2_class.js
+++ Tutorials for JS/module_2_OOP/module_2_lesson_2/module_2_lesson_2_class.js
@@ -1,1 +1,89 @@
 "use strict";
+
+// class Book {
+//          constructor() {
+//                    console.log("создание объекта book");
+//          }
+// }
+//
+// let book1 = new Book();
+//////////////////////////////////////////////////////////
+// class Book {
+//          constructor(title, author, price) {
+//                    this.title = title;
+//                    this.author = author;
+//                    this.price = price;
+//          }
+//          // getTitle() { return this.title; }
+//          // setPrice(pr) { this.price = pr; }
+// }
+//
+// let book1 = new Book("Муму", "Тургенев", 112);
+// console.log(book1);
+// // console.log(book1.getTitle());
+//////////////////////////////////////////////////////////
+//
+// function Book(title, author, price) {
+//          this.title = title;
+//          this.author = author;
+//          this.price = price;
+// }
+//
+// Book.prototype.getTitle = function() { return this.title; }
+// Book.prototype.setPrice = function(pr) { this.price = pr; }
+
+//////////////////////////////////////////////////////////
+// // Class Expression
+// let Book = class {
+//          constructor(title, author, price) {
+//                    this.title = title;
+//                    this.author = author;
+//                    this.price = price;
+//          };
+// }
+//
+// let Book = class BookClass {
+//          constructor(title, author, price) {
+//                    this.title = title;
+//                    this.author = author;
+//                    this.price = price;
+//          };
+// }
+//////////////////////////////////////////////////////////
+// let Book = class {
+//          constructor(title, author, price) {
+//                    this.title = title;
+//                    this.author = author;
+//                    this.price = price;
+//          };
+//          get titleBook() {return this.title; }
+//          set titleBook(value) { this.title = value; }
+// }
+// let book1 = new Book("Муму", "Тургенев", 112);
+//
+// console.log(book1.titleBook) //расширить геттеры на все поля
+//////////////////////////////////////////////////////////
+// class Book {
+//          pages = 123;
+//
+//          constructor(title, author, price) {
+//                    this.title = title;
+//                    this.author = author;
+//                    this.price = price;
+//          };
+// }
+//
+// let book1 = new Book("Муму", "Тургенев", 112);
+// console.log(book1);
+
+
+
+
+
+
+
+
+
+
+
+

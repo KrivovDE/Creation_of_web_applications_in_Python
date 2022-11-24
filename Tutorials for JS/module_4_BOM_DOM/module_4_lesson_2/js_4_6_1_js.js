d7793b034e642daf40d77b46673ad4bd37f93467
KrivovDE <55reigen55@mail.ru>
2022-11-24 18:40:00 +0300

    js module 4 dom node

---
+++ Tutorials for JS/module_4_BOM_DOM/module_4_lesson_2/js_4_6_1_js.js
@@ -0,0 +1,36 @@
+// Для каждого узла дерева список его дочерних элементов
+// можно получить через коллекцию childNodes:
+
+// for (let i = 0; i < document.body.childNodes.length; i++) {
+//     console.log( document.body.childNodes[i] );
+// }
+///////////////////////////////////////////////////////////////
+// for (let child of document.body.childNodes) {
+//     console.log( child );
+// }
+///////////////////////////////////////////////////////////////
+// let vel = document.documentElement;
+// let first = vel.firstChild;
+// let last = vel.lastChild;
+//
+// console.log( first );
+// console.log( last );
+// console.log( vel );
+///////////////////////////////////////////////////////////////
+// console.log(document.head.nextSibling);
+// console.log(document.body.previousSibling)
+// console.log(document.body.parentNode);
+///////////////////////////////////////////////////////////////
+
+
+
+
+///////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+

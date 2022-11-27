4c7f15924cc04a061393951843bfb33c7e0e7726
KrivovDE <55reigen55@mail.ru>
2022-11-27 20:05:26 +0300

    js module 7

---
+++ Tutorials for JS/module_7_JSON_Ajax/mod_7_lesson_1_Ajax/mod_7_less_1_1.js
@@ -0,0 +1,82 @@
+let person = {
+ firstName: "Andrey",
+ lastName: "Ivanov",
+ birthDate : "05.05.2000"
+}
+
+//        "{
+//          "firstName" : "Andrey",
+//          "lastName" : "Ivanov",
+//          "birthDate" : "04.05.2000"
+//         }"
+////////////////////////////////////////////////
+{
+ "firstName" : "Andrey",
+ "lastName" : "Ivanov",
+ "age" : 20,
+ "isStudent" : true
+}
+////////////////////////////////////////////////
+{
+ "firstName" : "Andrey",
+ "lastName" : "Ivanov",
+ "age" : 20,
+ "isStudent" : true,
+ "сontactInfo" : {
+ "phone" : "098-556-33-41",
+ "email" : "AndreyIvanov@gmail.com"
+ }
+}
+////////////////////////////////////////////////
+{
+ "firstName" : "Andrey",
+ "lastName" : "Ivanov",
+ "age" : 20,
+ "isStudent" : true,
+ "сontactInfo" : {
+ "phone" : "098-556-33-41",
+ "email" : "AndreyIvanov@gmail.com"
+ },
+ "disciplines" : [
+ "Programming", "Machine engineering", "English"
+ ]
+}
+////////////////////////////////////////////////
+
+
+
+
+
+
+////////////////////////////////////////////////
+
+
+
+
+////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

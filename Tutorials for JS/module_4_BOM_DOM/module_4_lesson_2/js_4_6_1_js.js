e4129b37f462dee929f9b9cce4ee6cabe4116fdd
KrivovDE <55reigen55@mail.ru>
2022-11-28 20:54:08 +0300

    js module 7,1

--- Tutorials for JS/module_4_BOM_DOM/module_4_lesson_2/js_4_6_1_js.js
+++ Tutorials for JS/module_4_BOM_DOM/module_4_lesson_2/js_4_6_1_js.js
@@ -1,5 +1,7 @@
 // Для каждого узла дерева список его дочерних элементов
 // можно получить через коллекцию childNodes:
+// let v = document.body.childNodes
+// console.log(typeof (v))
 
 // for (let i = 0; i < document.body.childNodes.length; i++) {
 //     console.log( document.body.childNodes[i] );
@@ -17,9 +19,9 @@
 // console.log( last );
 // console.log( vel );
 ///////////////////////////////////////////////////////////////
-// console.log(document.head.nextSibling);
-// console.log(document.body.previousSibling)
-// console.log(document.body.parentNode);
+console.log(document.head.nextSibling);
+console.log(document.body.previousSibling)
+console.log(document.body.parentNode);
 ///////////////////////////////////////////////////////////////
 
 
--- Tutorials for JS/module_7_JSON_Ajax/mod_7_lesson_1_Ajax/mod_7_less_1_1.js
+++ Tutorials for JS/module_7_JSON_Ajax/mod_7_lesson_1_Ajax/mod_7_less_1_1.js
@@ -1,59 +1,177 @@
-let person = {
- firstName: "Andrey",
- lastName: "Ivanov",
- birthDate : "05.05.2000"
-}
+// let person = {
+//  firstName: "Andrey",
+//  lastName: "Ivanov",
+//  birthDate : "05.05.2000"
+// }
 
 //        "{
 //          "firstName" : "Andrey",
 //          "lastName" : "Ivanov",
 //          "birthDate" : "04.05.2000"
 //         }"
+// ////////////////////////////////////////////////
+// {
+//  "firstName" : "Andrey",
+//  "lastName" : "Ivanov",
+//  "age" : 20,
+//  "isStudent" : true
+// }
+// ////////////////////////////////////////////////
+// {
+//  "firstName" : "Andrey",
+//  "lastName" : "Ivanov",
+//  "age" : 20,
+//  "isStudent" : true,
+//  "сontactInfo" : {
+//  "phone" : "098-556-33-41",
+//  "email" : "AndreyIvanov@gmail.com"
+//  }
+// }
+// ////////////////////////////////////////////////
+// {
+//  "firstName" : "Andrey",
+//  "lastName" : "Ivanov",
+//  "age" : 20,
+//  "isStudent" : true,
+//  "сontactInfo" : {
+//  "phone" : "098-556-33-41",
+//  "email" : "AndreyIvanov@gmail.com"
+//  },
+//  "disciplines" : ["Programming", "Machine engineering", "English"]
+// }
+// ////////////////////////////////////////////////
+
+// let person = {
+//                firstName: "Andrey",
+//                lastName: "Ivanov",
+//                age: 20,
+//                isStudent: true,
+//                contactInfo: {
+//                              "phone": "098-556-33-41",
+//                              "email": "AndreyIvanov@gmail.com"
+//                              },
+//                disciplines: ["Programming", "Machine engineering", "English"]
+//               }
+//
+// let jsonPerson = JSON.stringify(person);
+// document.write(`<h1 style="color: blue">` + jsonPerson + `</h1>`)
+//
+
 ////////////////////////////////////////////////
-{
- "firstName" : "Andrey",
- "lastName" : "Ivanov",
- "age" : 20,
- "isStudent" : true
+// Циклическая ссылка
+// let Kate = {
+//  name : "Kate"
+// }
+// let Dev = {
+//  name: "Dev"
+// }
+// Kate.parent = Dev;
+// Dev.child = Kate;
+// let falily = JSON.stringify(Dev);
+// console.log(falily);
+
+////////////////////////////////////////////////
+// let person = {
+//                firstName: "Andrey",
+//                lastName: "Ivanov",
+//                age: 333,
+//                isStudent: true,
+//                contactInfo: {
+//                              "phone": "098-556-33-41",
+//                              "email": "AndreyIvanov@gmail.com"
+//                              },
+//                disciplines: ["Programming", "Machine engineering", "English"]
+//               }
+//
+// function checkAge(key, value) {
+//     if (key === "age" && value >= 18) {
+//         return undefined;
+//     }
+//     return value;
+// }
+
+// // let jsonPerson2 = JSON.stringify(person, checkAge);
+// // document.write(`<h1 style="color: red">` + jsonPerson2 + `</h1>`)
+//
+// let jsonPerson3 = JSON.stringify(person, ["firstName", "lastName"]);
+// document.write(`<h1 style="color: mediumspringgreen">` + jsonPerson3 + `</h1>`)
+
+// let jsonPerson3 = JSON.stringify(person, ["firstName", "lastName"]);
+// document.write(`<h1 style="color: mediumspringgreen">` + jsonPerson3 + `</h1>`)
+// console.log(jsonPerson3)
+
+// let jsonPerson4 = JSON.stringify(person, null, 2);
+// document.write(`<h1 style="color: blueviolet">` + jsonPerson4 + `</h1>`)
+// console.log(jsonPerson4)
+////////////////////////////////////////////////
+// let personStr = '{' +
+//                 '"firstName": "Andrey", ' +
+//                 '"lastName": "Ivanov",' +
+//                 '"age": 20, ' +
+//                 '"isStudent": true, ' +
+//                 '"contactInfo": { ' +
+//                                     '"phone": "098-556-33-41", ' +
+//                                     '"email": "AndreyIvanov@gmail.com" }, ' +
+//                 '"disciplines": [ "Programming", "Machine engineering", "English" ]' +
+//                 '}'
+//
+// let person = JSON.parse(personStr);
+// console.log(typeof(person))
+// console.log(person)
+//
+// alert(person.firstName)
+// alert(person.contactInfo.phone);
+////////////////////////////////////////////////
+// let personStr = '{' +
+//                 '"firstName": "Andrey", ' +
+//                 '"lastName": "Ivanov",' +
+//                 '"age": 20, ' +
+//                 '"isStudent": true, ' +
+//                 '"contactInfo": { ' +
+//                                     '"phone": "098-556-33-41", ' +
+//                                     '"email": "AndreyIvanov@gmail.com" }, ' +
+//                 '"disciplines": [ "Programming", "Machine engineering", "English" ]' +
+//                 '}'
+//
+// function CheckIsStudent(key, value) {
+//     if (key === "isStudent" && value === true) {
+//         return undefined;
+//     }
+// return value;
+// }
+//
+// let person2 = JSON.parse(personStr, CheckIsStudent);
+// alert(person2.isStudent);
+//
+
+////////////////////////////////////////////////
+
+let model = {
+            name: "BMW",
+            autopilot : undefined,
+            toJSON(){
+                    let jsonStr = `{"name": "${this.name}", "autopilot": `;
+                    if(this.autopilot === undefined){
+                        jsonStr += `"Not"}`
+                    }
+                    else{
+                        jsonStr += `"${this.autopilot}"`
+                    }
+            return jsonStr;
+            }
+
 }
-////////////////////////////////////////////////
-{
- "firstName" : "Andrey",
- "lastName" : "Ivanov",
- "age" : 20,
- "isStudent" : true,
- "сontactInfo" : {
- "phone" : "098-556-33-41",
- "email" : "AndreyIvanov@gmail.com"
- }
+let car = {
+            color: "Black",
+            date : new Date(2022, 2, 24),
+            model
 }
-////////////////////////////////////////////////
-{
- "firstName" : "Andrey",
- "lastName" : "Ivanov",
- "age" : 20,
- "isStudent" : true,
- "сontactInfo" : {
- "phone" : "098-556-33-41",
- "email" : "AndreyIvanov@gmail.com"
- },
- "disciplines" : [
- "Programming", "Machine engineering", "English"
- ]
-}
-////////////////////////////////////////////////
+
+let carJSON = JSON.stringify(car)
+alert(carJSON)
+document.write(`<h1 style="color: darkorange">` + carJSON + `</h1>`)
 
 
-
-
-
-
-////////////////////////////////////////////////
-
-
-
-
-////////////////////////////////////////////////
 
 
 

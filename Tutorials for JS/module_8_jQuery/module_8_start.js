b1734ecaa2dbf983fcddc8fe16d3443b6dd340ac
KrivovDE <55reigen55@mail.ru>
2022-12-20 20:18:04 +0300

    mod_8

---
+++ Tutorials for JS/module_8_jQuery/module_8_start.js
@@ -0,0 +1,106 @@
+// jQuery(document).ready(function(){
+//  //ваш код
+// });
+//
+// $(document).ready(function(){
+//  //ваш код
+// });
+//
+// // используем стрелочную функцию
+// $(document).ready(()=>{
+//  $('.block).addClass('active');
+//  //ваш код
+// });
+//
+// // укороченный вариант
+// $(function(){
+//  $('.nav-link').addClass('active');
+//  //ваш код
+// });
+//
+// // используем стрелочную функцию
+// $(() => {
+//  $('.block).attr('data-text', 'Test Block');
+//  //ваш код
+// });
+///////////////////////////////////////////////////////////
+
+// $(':button').prop('disabled', true);
+//
+// $('button:disabled').prop('disabled', false);
+//
+// $(':password').css('border', '2px solid red');
+///////////////////////////////////////////////////////////
+// $('#block').css('color', 'red');
+//
+// <div id="block" style="color: red">Lorem, ipsum dolor </div>
+
+// ///////////////////////////////////////////////////////////
+// $('.example').css({
+//     color: 'red',
+//     backgroundColor: 'pink',
+//     fontSize: '18px'
+// });
+
+// <div class="example" style="color: red; background-color: pink; font-size: 18px">
+//     Lorem ipsum dolor sit.
+// </div>
+// <div class="example" style="color: red; background-color: pink; font-size: 18px">
+//     Dolores numquam architecto velit.
+// </div>
+
+// ///////////////////////////////////////////////////////////
+// $('a.active').removeClass('active');
+// $('.menu li a').addClass('active');
+
+///////////////////////////////////////////////////////////
+// $('.popup-close').closest('.popup-wrapper').fadeOut(400);
+//
+// ///////////////////////////////////////////////////////////
+// let section = $(this).parents('section');
+//
+// $('.menu li a').click(function () {
+// 	$(this).closest('.menu').find('.active').removeClass('active');
+// 	$(this).addClass('active');
+// })
+///////////////////////////////////////////////////////////
+// $('селектор').on('событие', function(){
+//  // ваш код
+// });
+//
+// $('a.active').on('click', function(){
+//     $(this).removeClass('active');
+// })
+// //
+// $('селектор').on('mousedown', function(){ });
+// вы можете написать
+// $('селектор').mousedown( function(){ });
+///////////////////////////////////////////////////////////
+// https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+
+

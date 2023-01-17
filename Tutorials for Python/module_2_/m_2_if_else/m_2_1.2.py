ee28caabd6265423a2a2b671b108ccb4d1f57c1e
KrivovDE <55reigen55@mail.ru>
2023-01-17 22:12:20 +0300

    m_2_1_theory.1

--- Tutorials for Python/module_2_/m_2_if_else/m_2_1.2.py
+++ Tutorials for Python/module_2_/m_2_if_else/m_2_1.2.py
@@ -1,50 +1,54 @@
-x = int(input())
-y = int(input())
-if x > 0:
-    if y > 0:
-        print('Первая четверть')
-    else:
-        print('Четвертая четверть')
-else:
-    if y > 0:
-        print('Вторая четверть')
-    else:
-        print('Третья четверть')
+# x = int(input())
+# y = int(input())
+# if x > 0:
+#     if y > 0:
+#         print('Первая четверть')
+#     else:
+#         print('Четвертая четверть')
+# else:
+#     if y > 0:
+#         print('Вторая четверть')
+#     else:
+#         print('Третья четверть')
+# # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# grade = int(input('Введите вашу отметку по 100-балльной системе: '))
+#
+# if grade >= 90:
+#     print(5)
+# else:
+#     if grade >= 80:
+#         print(4)
+#     else:
+#         if grade >= 70:
+#             print(3)
+#         else:
+#             if grade >= 60:
+#                 print(2)
+#             else:
+#                 print(1)
+# # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# grade = int(input('Введите вашу отметку: '))
+#
+# if grade >= 90:
+#     print(5)
+# elif grade >= 80:
+#     print(4)
+# elif grade >= 70:
+#     print(3)
+# elif grade >= 60:
+#     print(2)
+# else:
+#     print(1)
 # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-grade = int(input('Введите вашу отметку по 100-балльной системе: '))
 
-if grade >= 90:
-    print(5)
-else:
-    if grade >= 80:
-        print(4)
-    else:
-        if grade >= 70:
-            print(3)
-        else:
-            if grade >= 60:
-                print(2)
-            else:
-                print(1)
-# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-grade = int(input('Введите вашу отметку: '))
+# [если истина] if [выражение] else [если ложь]
 
-if grade >= 90:
-    print(5)
-elif grade >= 80:
-    print(4)
-elif grade >= 70:
-    print(3)
-elif grade >= 60:
-    print(2)
-else:
-    print(1)
-# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 x, y = 25, 50
 big = x if x < y else y
 
-num1 = 5
-num2 = 10
+age = 18
+accessAllowed = True if age >= 18 else False
+print(accessAllowed)
 
-print("num1 больше, чем num2" if num1 > num2 else "num1 меньше, чем num2" if num1 == num2 else "Оба числа не равны")
+accessAllowed = age >= 18

dc5fa9328c41097487a41951511cf74b72a3007d
KrivovDE <55reigen55@mail.ru>
2023-01-19 10:43:32 +0300

    m_2_1_4

--- Tutorials for Python/module_2_/m_2_if_else/m_2_1.2.py
+++ Tutorials for Python/module_2_/m_2_if_else/m_2_1.2.py
@@ -10,7 +10,7 @@
 #         print('Вторая четверть')
 #     else:
 #         print('Третья четверть')
-# # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # grade = int(input('Введите вашу отметку по 100-балльной системе: '))
 #
 # if grade >= 90:
@@ -26,7 +26,7 @@
 #                 print(2)
 #             else:
 #                 print(1)
-# # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # grade = int(input('Введите вашу отметку: '))
 #
 # if grade >= 90:
@@ -47,8 +47,11 @@
 x, y = 25, 50
 big = x if x < y else y
 
-age = 18
-accessAllowed = True if age >= 18 else False
-print(accessAllowed)
+# age = 11
+#
+# # accessAllowed = True if age >= 18 else False
+#
+# accessAllowed = age >= 18
+#
+# print(accessAllowed)
 
-accessAllowed = age >= 18
---
+++ Tutorials for Python/module_2_/m_2_if_else/m_2_1.3.py
@@ -0,0 +1,105 @@
+# USERNAMES = {'peter', '123', 'progamer'}
+#
+#
+# def create_new_user(username, password):
+#     global USERNAMES
+#     USERNAMES.add(username)
+#
+#
+# def register_user():
+#     username = input()
+#     password = input()
+#
+#     if len(username) <= 20:
+#         if len(username) > 3:
+#             if username in USERNAMES:
+#                 print("User with this username already exists")
+#             else:
+#                 if len(password) > 3:
+#                     create_new_user(username, password)
+#                     print("New user is created")
+#                 else:
+#                     print("The password is too short")
+#         else:
+#             print("Username is too short")
+#     else:
+#         print("Username is too long")
+
+
+# if __name__ == '__main__':
+#     register_user()
+# # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# if len(password) <= 3:
+#     print("The password is too short")
+#     return
+# else:
+#     create_new_user(username, password)
+#     print("New user is created")
+#
+# if len(password) <= 3:
+#     print("The password is too short")
+#     return
+# create_new_user(username, password)
+# print("New user is created")
+# # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#
+# def register_user():
+#     username = input()
+#     password = input()
+#
+#     if len(username) > 20:
+#         print("Username is too long")
+#         return
+#
+#     if len(username) <= 3:
+#         print("Username is too short")
+#         return
+#
+#     if username in USERNAMES:
+#         print("User with this username already exists")
+#         return
+#
+#     if len(password) <= 3:
+#         print("The password is too short")
+#         return
+#
+
+    # create_new_user(username, password)
+#     # print("New user is created")
+# #
+# # # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# def is_username_valid(username):
+#     if len(username) > 20:
+#         print("Username is too long")
+#         return False
+#
+#     if len(username) <= 3:
+#         print("Username is too short")
+#         return False
+#
+#     if username in USERNAMES:
+#         print("User with this username already exists")
+#         return False
+#
+#     return True
+#
+#
+# def is_password_valid(password):
+#     if len(password) <= 3:
+#         print("The password is too short")
+#         return False
+#     return True
+#
+#
+# def register_user():
+#     username = input()
+#     password = input()
+#
+#     if not is_username_valid(username):
+#         return
+#
+#     if not is_password_valid(password):
+#         return
+#
+#     create_new_user(username, password)
+#     print("New user is created")

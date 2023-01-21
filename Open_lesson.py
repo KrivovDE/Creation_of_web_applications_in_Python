c58141ce2eecced077927717a688240793c50880
KrivovDE <55reigen55@mail.ru>
2023-01-21 14:57:27 +0300

    jpen_lesson

---
+++ Open_lesson.py
@@ -0,0 +1,64 @@
+# x = int(input('Введите число - '))
+#
+# if x < 0:
+#     print("x отрицательное число")
+# if x >= 0:
+#     print("x неотрицательное число")
+
+#
+# speed_const = 60
+# speed_car = float(input('Введите скорость автомобиля: '))
+#
+# if speed_car <= 60:
+#     print('Скоростной режим не нарушен')
+# elif 60 > speed_car <= 80:
+#     print('Превышение скорости от 20 до 40 км/ч - штраф 500 рублей')
+# elif 80 > speed_car <= 120:
+#     print('Превышение скорости от 40 до 60 км/ч - штраф 1000 рублей')
+# else:
+#     print('Превышение скорости от 80 км/ч - штраф 5000 рублей')
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+USERNAMES = {'peter', '123', 'progamer'}
+
+
+def create_new_user(username, password):
+    global USERNAMES
+    USERNAMES.add(username)
+
+
+def register_user():
+    username = input()
+    password = input()
+
+    if len(username) <= 20:
+        if len(username) > 3:
+            if username in USERNAMES:
+                print("User with this username already exists")
+            else:
+                if len(password) > 3:
+                    create_new_user(username, password)
+                    print("New user is created")
+                else:
+                    print("The password is too short")
+        else:
+            print("Username is too short")
+    else:
+        print("Username is too long")
+
+
+if __name__ == '__main__':
+    register_user()

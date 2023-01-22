e7ab480a5672935f5b97237146bd8abb458d3f3e
KrivovDE <55reigen55@mail.ru>
2023-01-22 22:02:39 +0300

    m_2_2+add_DZ

--- Tutorials for Python/module_2_/m_2_if_else/m_2_1.3.py
+++ Tutorials for Python/module_2_/m_2_if_else/m_2_1.3.py
@@ -1,33 +1,33 @@
-# USERNAMES = {'peter', '123', 'progamer'}
-#
-#
-# def create_new_user(username, password):
-#     global USERNAMES
-#     USERNAMES.add(username)
-#
-#
-# def register_user():
-#     username = input()
-#     password = input()
-#
-#     if len(username) <= 20:
-#         if len(username) > 3:
-#             if username in USERNAMES:
-#                 print("User with this username already exists")
-#             else:
-#                 if len(password) > 3:
-#                     create_new_user(username, password)
-#                     print("New user is created")
-#                 else:
-#                     print("The password is too short")
-#         else:
-#             print("Username is too short")
-#     else:
-#         print("Username is too long")
+USERNAMES = {'peter', '123', 'progamer'}
 
 
-# if __name__ == '__main__':
-#     register_user()
+def create_new_user(username, password):
+    global USERNAMES
+    USERNAMES.add(username)
+
+
+def register_user():
+    username = input()
+    password = input()
+
+    if len(username) <= 20:
+        if len(username) > 3:
+            if username in USERNAMES:
+                print("User with this username already exists")
+            else:
+                if len(password) > 3:
+                    create_new_user(username, password)
+                    print("New user is created")
+                else:
+                    print("The password is too short")
+        else:
+            print("Username is too short")
+    else:
+        print("Username is too long")
+
+
+if __name__ == '__main__':
+    register_user()
 # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # if len(password) <= 3:
 #     print("The password is too short")
---
+++ Tutorials for Python/module_2_/m_2_try_except/m_2_2_practict.py
@@ -0,0 +1,32 @@
+# try:
+#     f = open('abc.txt')
+#     r = f.read(1)
+#     f.close()
+# except FileNotFoundError:
+#     print('File Not Found')
+
+
+
+# a = float(input("a = "))
+# b = float(input("b = "))
+#
+# c = (a * b) ** 0.5
+#
+# print(f'Среднее геометрическое = {c}')
+
+
+
+try:
+    a = float(input("a = "))
+    b = float(input("b = "))
+
+    if a * b >= 0:
+        c = (a * b) ** 0.5
+    else:
+        raise ValueError("Невозможно определить среднее геометрическое введенных чисел.")
+    print(f'Среднее геометрическое = {c}')
+
+except ValueError as err:
+    print("Ошибка:", err, "Проверьте введенные числа")
+except Exception as err:
+    print("Ошибка:", err)

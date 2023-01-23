41c65a3a039ce50b506a4c481dbf9459b2bb8d06
KrivovDE <55reigen55@mail.ru>
2023-01-23 14:57:23 +0300

    m_2_3

--- Tutorials for Python/module_2_/m_2_while_for/m_2_3.py
+++ Tutorials for Python/module_2_/m_2_while_for/m_2_3.py
@@ -7,15 +7,73 @@
 # print(s)
 
 
-z = 0
-v = 10
+# z = 0
+# v = -10
+#
+# while v < 100:
+#     if v == 0:
+#         break
+#     z += 1/v
+#     v = v + 1
+# else:
+#     print("Сумма вычислена корретно")
+# print(z)
 
-while v < 100:
-    if v == 0:
-        break
-    z += 1/v
-    v = v + 1
-else:
-    print("Сумма вычислена корретно")
-print(z)
 
+# a = 0
+# b = -5
+#
+# while b < 4:
+#     b = b + 1
+#     if b == 0:
+#         continue
+#     print(b)
+#     a += 1/b
+# print(a)
+
+
+#
+# for x in 1, 5, 2, 4:
+#     print(x**2)
+
+# for x in range(1, 5, 1):
+#     print(x)
+#
+#
+# s = 0
+#
+# for i in range(1, 1001, 1):
+#     s += 1/i
+# print(s)
+
+
+# k = 0.5; b = 2
+# lst = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+# for x in lst:
+#     print(x*k+b)
+
+    # line = input("Enter some string: ")
+    #
+    # for c in line:
+    #     print(c)
+
+# for i in range(1, 10):
+#     for j in range(1, 10):
+#         print(i * j, end="\t")
+#     print("\n")
+
+# floor = 1
+# energy = 70
+# print(f'Я на {floor} этаже')
+#
+# while floor != 5:
+#     step = 0
+#     while step != 20:
+#         step += 1
+#         energy -= 1
+#         if energy == 0:
+#             print('Я устал, я немного отдохну')
+#             energy += 70
+#     floor += 1
+#     print(f'Сейчас я на {floor} этаже')
+# print('Ghbitk')

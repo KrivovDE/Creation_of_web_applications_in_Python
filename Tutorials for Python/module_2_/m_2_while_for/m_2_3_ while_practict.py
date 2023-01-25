a573d36283469dbb0f11515e079f5fb0316807c7
KrivovDE <55reigen55@mail.ru>
2023-01-25 09:48:23 +0300

    m_2_3_ while_practict_3.py

--- Tutorials for Python/module_2_/m_2_while_for/m_2_3_ while_practict.py
+++ Tutorials for Python/module_2_/m_2_while_for/m_2_3_ while_practict.py
@@ -57,29 +57,86 @@
 # Формат вывода:
 #   Студенту надо попросить ХХХ.ХХ рублей
 
-ed, exp = 10000, 12000
+# ed, exp = 10000, 12000
+#
+# m = 1
+# d = exp - ed
+# while m < 10:
+#     m += 1
+#     exp *= 1.03
+#     d += exp - ed
+#
+# d = round(d, 2)
+# print('Студенту надо попросить', d, 'рублей.')
+#
+#
+# educational_grant = float(input('Введите размер дохода: '))
+# expenses = float(input('Введите сумму расходов: '))
+# time_period = int(input('Введите количество месяцов: '))
+#
+# inflation = 1.03
+# month = 1
+# if educational_grant > expenses:
+#     print('по условию расходы превышают доходы, проверте введенные данные')
+# else:
+#     credit = expenses - educational_grant
+#     while month < time_period:
+#         month += 1
+#         expenses *= inflation
+#         credit += expenses - educational_grant
+#     print('------Студенту надо попросить', round(credit, 2), 'рублей.')
 
-m = 1
-d = exp - ed
-while m < 10:
-    m += 1
-    exp *= 1.03
-    d += exp - ed
 
-d = round(d, 2)
-print('Студенту надо попросить', d, 'рублей.')
 
-educational_grant = float(input('Введите размер дохода: '))
-expenses = float(input('Введите сумму расходов: '))
 
-inflation = 1.03
-month = 1
-while month < 10:
-    month += 1
-    expenses += expenses * inflation
-credit = round(expenses - educational_grant*10, 2)
-print('------Студенту надо попросить', credit, 'рублей.')
+# По номеру месяца вывести кол-во дней в нем (без указания названия месяца, в феврале 28 дней)
+# Результат проверки вывести на консоль
+# Если номер месяца некорректен - сообщить об этом
 
+user_input = input("Введите, пожалуйста, номер месяца: ")
+month = int(user_input)
+print('Вы ввели', month)
+if month == 1:
+    print('31 день')
+elif month == 2:
+    print('28 дней')
+elif month == 3:
+    print('31 день')
+elif month == 4:
+    print('30 дней')
+elif month == 5:
+    print('31 день')
+elif month == 6:
+    print('30 дней')
+elif month == 7:
+    print('31 день')
+elif month == 8:
+    print('31 день')
+elif month == 9:
+    print('30 дней')
+elif month == 10:
+    print('31 день')
+elif month == 11:
+    print('30 дней')
+elif month == 12:
+    print('31 день')
+else:
+    print('Такого месяца нет')
+
+
+months_31 = [1, 3, 5, 7, 8, 10, 12]
+months_30 = [4, 6, 9, 11]
+months_february = [2]
+
+choice = int(input("Введите, пожалуйста, номер месяца: "))
+if choice in months_31:
+    print('--31 день')
+elif choice in months_30:
+    print('--30 дней')
+elif choice in months_february:
+    print('--28 дней')
+else:
+    print('--Такого месяца нет')
 
 
 

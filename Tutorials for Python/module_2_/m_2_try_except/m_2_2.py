e9f82466b7feb0f6bcfb5b57d7ca8c00bc6ebc97
KrivovDE <55reigen55@mail.ru>
2023-02-04 01:09:53 +0300

    m+2

--- Tutorials for Python/module_2_/m_2_try_except/m_2_2.py
+++ Tutorials for Python/module_2_/m_2_try_except/m_2_2.py
@@ -1,12 +1,6 @@
 # x = input("x: ")
 # y = input("y: ")
 #
-# x = int(x)
-# y = int(y)
-#
-# res = x / y
-# print(res)
-#
 # try:
 #     x = int(x)
 #     y = int(y)
@@ -14,64 +8,65 @@
 #     res = x / y
 # except ZeroDivisionError:
 #     res = "деление на ноль"
+# except ValueError:
+#     res = "одно из введенных значений не число"
 # print(res)
 # # # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 #
 #
-# # except (ZeroDivisionError, ValueError):
-# #     res = "деление на ноль или нечисловое значение"
-# #
-# # except ZeroDivisionError:
-# #     res = "деление на ноль"
-# # except ValueError:
-# #     res = "одно из введенных значений не число"
+# except (ZeroDivisionError, ValueError):
+#     res = "деление на ноль или нечисловое значение"
+#
+# except ZeroDivisionError:
+#     res = "деление на ноль"
+# except ValueError:
+#     res = "одно из введенных значений не число"
 # # # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 #
-# # def getValues():
-# #     x = input("x: ")
-# #     y = input("y: ")
-# #     try:
-# #         x = int(x)
-# #         y = int(y)
-# #         return x, y
-# #     except ValueError as v:
-# #         print(v)
-# #         return 0, 0
-# #     finally:
-# #         print("finally выполняется до return")
-# #
-# #
-# # x, y = getValues()
-# # print(x, y)
+# def getValues():
+#     x = input("x: ")
+#     y = input("y: ")
+#     try:
+#         x = int(x)
+#         y = int(y)
+#         return x, y
+#     except ValueError as v:
+#         print(v)
+#         return 0, 0
+#     finally:
+#         print("finally выполняется до return")
+#
+# x, y = getValues()
+# print(x, y)
 # # # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 #
-# x = input("x: ")
-# y = input("y: ")
-# try:
-#     x = int(x)
-#     y = int(y)
-#
-#     res = x / y
-# except:
-#     print("Произошло исключение")
-# else:
-#     print("Исключений не произошло")
-# finally:
-#     print("Блок finally выполняется всегда")
-#
+x = input("x: ")
+y = input("y: ")
+try:
+    x = int(x)
+    y = int(y)
+
+    res = x / y
+except:
+    print("Произошло исключение")
+else:
+    print("Исключений не произошло")
+finally:
+    print("Блок finally выполняется всегда")
+
 # print(res)
 # # # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-# print("Куда ты скачешь, гордый конь,")
-# print("И где опустишь ты копыта?")
-# print("О мощный властелин судьбы!")
-# 1/0
-# print("Не так ли ты над самой бездной")
-# print("На высоте, уздой железной")
-# print("Россию поднял на дыбы?")
-#
-# raise ZeroDivisionError("Деление на ноль")
-# e = ZeroDivisionError("Деление на ноль")
-# raise e
+print("Куда ты скачешь, гордый конь,")
+print("И где опустишь ты копыта?")
+print("О мощный властелин судьбы!")
+e = ZeroDivisionError("Деление на ноль")
+raise e
+print("Не так ли ты над самой бездной")
+print("На высоте, уздой железной")
+print("Россию поднял на дыбы?")
+
+
+
 # # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 # try:
 #     f = open("Some_file.txt")
@@ -90,16 +85,17 @@
 #         print("Деление на ноль")
 # except ValueError as z:
 #     print(z)
-# # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# # # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 # def div(a, b):
 #     try:
 #         return a / b
 #     except ZeroDivisionError:
 #         return "Деление на ноль"
-#
-#
+# #
+# #
 # res = None
+#
 # try:
 #     x = int(input())
 #     y = int(input())
@@ -109,21 +105,21 @@
 #
 # print(res)
 # # # # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
 #
-#
-# class ExceptionPrintSendData(Exception):
-#     """Класс исключения при отправке данных принтеру"""
-#
-#
-# class PrintData:
-#     def print(self, data):
-#         self.send_data(data)
-#         print(f"печать: {str(data)}")
-#
-#     def send_data(self, data):
-#         if not self.send_to_print(data):
-#             raise ExceptionPrintSendData("принтер не отвечает")
+class ExceptionPrintSendData(Exception):
+    """Класс исключения при отправке данных принтеру"""
 
 
+class PrintData:
+    def print(self, data):
+        self.send_data(data)
+        print(f"печать: {str(data)}")
+
+    def send_data(self, data):
+        if not self.send_to_print(data):
+            raise ExceptionPrintSendData("принтер не отвечает")
+
+#
 
 

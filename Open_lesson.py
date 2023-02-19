476ac2c927fb23a37a68ca9575e4cea249767d37
KrivovDE <55reigen55@mail.ru>
2023-02-19 11:28:33 +0300

    dz_m_4_k

--- Open_lesson.py
+++ Open_lesson.py
@@ -1,48 +1,48 @@
-x = int(input('Введите число - '))
+# x = int(input('Введите число - '))
+#
+# if x < 0:
+#     print("x отрицательное число")
+# else:
+#     print("x неотрицательное число")
+#
 
-if x < 0:
-    print("x отрицательное число")
-if x >= 0:
-    print("x неотрицательное число")
-
-
-
-speed_car = float(input('Введите скорость автомобиля: '))
-
-if speed_car <= 60:
-    print('Скоростной режим не нарушен')
-elif 60 > speed_car <= 80:
-    print('Превышение скорости от 20 до 40 км/ч - штраф 500 рублей')
-elif 80 > speed_car <= 120:
-    print('Превышение скорости от 40 до 60 км/ч - штраф 1000 рублей')
-else:
-    print('Превышение скорости от 80 км/ч - штраф 5000 рублей')
-
+#
+# speed_car = float(input('Введите скорость автомобиля: '))
+#
+# if speed_car <= 60:
+#     print('Скоростной режим не нарушен')
+# elif 60 > speed_car <= 80:
+#     print('Превышение скорости от 20 до 40 км/ч - штраф 500 рублей')
+# elif 80 > speed_car <= 120:
+#     print('Превышение скорости от 40 до 60 км/ч - штраф 1000 рублей')
+# else:
+#     print('Превышение скорости от 80 км/ч - штраф 5000 рублей')
+#
 
 
 speed_const = int(input('Введите разрешенную скорость: '))
 
+while True:
+    speed_car = float(input('Введите скорость автомобиля: '))
 
-speed_car = float(input('Введите скорость автомобиля: '))
+    difference = speed_car - speed_const
 
-difference = speed_car - speed_const
+    if difference <= 0:
+        print('Скоростной режим не нарушен')
+    elif difference <= 40:
+        print('Превышение скорости от 20 до 40 км/ч - штраф 500 рублей')
+    elif difference <= 60:
+        print('Превышение скорости от 40 до 60 км/ч - штраф 1000 рублей')
+    else:
+        print('Превышение скорости от 80 км/ч - штраф 5000 рублей')
 
-if difference <= 0:
-    print('Скоростной режим не нарушен')
-elif difference <= 40:
-    print('Превышение скорости от 20 до 40 км/ч - штраф 500 рублей')
-elif difference <= 60:
-    print('Превышение скорости от 40 до 60 км/ч - штраф 1000 рублей')
-else:
-    print('Превышение скорости от 80 км/ч - штраф 5000 рублей')
-
-
-USERNAMES = {'peter', '123', 'progamer'}
-
-
-def create_new_user(username, password):
-    global USERNAMES
-    USERNAMES.add(username)
+#
+# USERNAMES = {'peter', '123', 'progamer'}
+#
+#
+# def create_new_user(username, password):
+#     global USERNAMES
+#     USERNAMES.add(username)
 
 # #
 # def register_user():
---
+++ Tutorials for Python/module_4_/completed_tasks_DZ_Py_mod_4_function/DZ_Py_Mod_4_function_IV.py
@@ -0,0 +1,79 @@
+# Задание 1
+# Напишите функцию, которая отображает пустой или заполненный квадрат из некоторого символа.
+# Функция принимает в качестве параметров: длину стороны квадрата, символ и переменную логического типа:
+# ■ если она равна True, квадрат заполненный;
+# ■ если False, квадрат пустой.
+
+def square(side_length, char, fill):
+    if fill:
+        print(*[char * side_length for i in range(side_length)], sep='\n')
+    else:
+        res = [char + ' ' * (side_length - 2) + char for i in range(side_length - 2)]
+        res.append(char * side_length)
+        res.insert(0, char * side_length)
+        print(*res, sep='\n')
+
+
+square(5, '#', False)
+square(6, '&', True)
+
+# Задание 2
+# Напишите функцию, которая возвращает произведение чисел в указанном диапазоне.
+# Границы диапазона передаются в качестве параметров. Если границы диапазона перепутаны (например, 5 — верхняя граница,
+# 25— нижняя граница), их нужно поменять местами.
+
+def multiply(num1, num2):
+    res = 1
+    if num1 > num2:
+        num1, num2 = num2, num1
+    while not num1 > num2:
+        res *= num1
+        num1 += 1
+    return res
+
+
+print(multiply(int(input('Enter num1: ')), int(input('Enter num2: '))))
+
+# Задание 3
+# Напишите функцию, которая считает количество цифр в числе. Число передаётся в качестве параметра.
+# Из функции нужно вернуть полученное количество цифр. Например, если передали 3456, количество цифр будет 4.
+#
+# def quan_digit(num):
+#     return len(str(abs(num)))
+#
+#
+# print(quan_digit(int(input('Enter number: '))))
+
+# Задание 4
+# Напишите функцию, которая проверяет является ли число палиндромом. Число передаётся в качестве параметра.
+# Если число палиндром нужно вернуть из функции true, иначе false.
+# «Палиндром» — это число, у которого первая часть цифр равна второй перевернутой части цифр.
+# Например, 123321 — палиндром (первая часть 123, вторая 321, которая после переворота становится 123),
+# 546645 — палиндром, а 421987 — не палиндром.
+
+def palindrom(num):
+    lst1 = [*str(num)]
+    lst2 = lst1.copy()
+    lst2.reverse()
+    return lst1 == lst2
+
+
+print(palindrom(int(input('Enter number: '))))
+
+# Задание 5
+# Напишите функцию, которая отображает горизонтальную или вертикальную линию из некоторого символа.
+# Функция принимает в качестве параметра: длину линии, направление, символ.
+
+def line(leng, direct, char):
+    if direct == 'hor':
+        print(*[char for i in range(leng)])
+    elif direct == 'ver':
+        print(*[char for i in range(leng)], sep='\n')
+
+
+quantity = int(input('Enter length of line: '))
+direction = input('Choose direction (hor / ver): ')
+character = input('Enter symbol: ')
+
+line(quantity, direction, character)
+

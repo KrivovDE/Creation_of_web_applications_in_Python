7251fb5e68894fe71545638469a3fcf2d8c29950
KrivovDE <55reigen55@mail.ru>
2023-03-07 09:21:50 +0300

    add_static_m_5_ref

--- Tutorials for Python/module_5_/m_5_sorting_algorithms/m_5_bubble_sort.py
+++ Tutorials for Python/module_5_/m_5_sorting_algorithms/m_5_bubble_sort.py
@@ -1,7 +1,7 @@
 
-nums_1 = [5, 2, 1, 8, 4]
-nums_2 = [5, 2, 1, 8, 4]
-nums_3 = [5, 2, 1, 8, 4]
+nums_1 = [5, 2, 1, 8, 4]*10
+nums_2 = [5, 2, 1, 8, 4]*10
+nums_3 = [5, 2, 1, 8, 4]*10
 
 
 def bubble_sort_1(_nums):
@@ -11,11 +11,11 @@ def bubble_sort_1(_nums):
                 _nums[j], _nums[j+1] = _nums[j+1], _nums[j]
 
 
-bubble_sort_1(nums_1)
-print(nums_1)
+# bubble_sort_1(nums_1)
+# print(nums_1)
 
-
-# оптимизация 1
+#
+# # оптимизация 1
 def bubble_sort_2(_nums):
     swapped = True
     while swapped:
@@ -24,29 +24,29 @@ def bubble_sort_2(_nums):
             if _nums[i] > _nums[i + 1]:
                 _nums[i], _nums[i + 1] = _nums[i + 1], _nums[i]
                 swapped = True
-
-
-bubble_sort_2(nums_2)
-print(nums_2)
-
-
-# оптимизация 2
+#
+#
+# bubble_sort_2(nums_2)
+# print(nums_2)
+#
+#
+# # оптимизация 2
 def bubble_sort_3(_nums):
     has_swapped = True
     num_of_iterations = 0
-    while(has_swapped):
+    while has_swapped:
         has_swapped = False
         for i in range(len(_nums) - num_of_iterations - 1):
             if _nums[i] > _nums[i+1]:
                 _nums[i], _nums[i+1] = _nums[i+1], _nums[i]
                 has_swapped = True
         num_of_iterations += 1
-
-
-bubble_sort_3(nums_3)
-print(nums_3)
-
-
+#
+#
+# bubble_sort_3(nums_3)
+# print(nums_3)
+#
+#
 
 if __name__ == '__main__':
     import timeit
@@ -55,7 +55,4 @@ if __name__ == '__main__':
     print(timeit.timeit('bubble_sort_1(nums_1)', globals=globals()))
     print(timeit.timeit('bubble_sort_2(nums_2)', globals=globals()))
     print(timeit.timeit('bubble_sort_3(nums_3)', globals=globals()))
-
-
-
 

5657a4758dcbbdbd7c8481bf2e82f6a8a22da9c5
KrivovDE <55reigen55@mail.ru>
2023-03-17 02:01:15 +0300

    m_7_2

--- Tutorials for Python/module_7/m_7.py
+++ Tutorials for Python/module_7/m_7.py
@@ -1,3 +1,8 @@
+# import os
+# abs_path = os.path.abspath('my_file')
+# print(abs_path)
+
+
 # file = open('/Users/ШАГ/Course creating web applications with Python/Tutorials for Python/module_7/my_file.txt')
 #
 #
@@ -50,20 +55,54 @@
 # file = open("out.txt", "a")
 # file.writelines(["Hello1\n", "Hello2\n"])
 # _____________________________________________________
-
+# import pickle
+#
+# books = [
+#     ("Евгений Онегин", "Пушкин А.С.", 200),
+#     ("Муму", "Тургенев И.С.", 250),
+#     ("Мастер и Маргарита", "Булгаков М.А.", 500),
+#     ("Мертвые души", "Гоголь Н.В.", 190)
+# ]
+#
+# try:
+#     file = open("out.bin", "wb")
+#     try:
+#         pickle.dump(books, file)
+#
+#         bs = pickle.load(file)
+#         print(bs)
+#     finally:
+#         file.close()
+# except FileNotFoundError:
+#     print('Невозможно открыть файл')
 
 # _____________________________________________________
-
+# import pickle
+# book1 = ["Евгений Онегин", "Пушкин А.С.", 200]
+# book2 = ["Муму", "Тургенев И.С.", 250]
+# book3 = ["Мастер и Маргарита", "Булгаков М.А.", 500]
+# book4 = ["Мертвые души", "Гоголь Н.В.", 190]
+# try:
+#     file = open("out.bin", "wb")
+#     try:
+#         pickle.dump(book1, file)
+#         pickle.dump(book2, file)
+#         pickle.dump(book3, file)
+#         pickle.dump(book4, file)
+#
+#         # b1 = pickle.load(file)
+#         # b2 = pickle.load(file)
+#         # b3 = pickle.load(file)
+#         # b4 = pickle.load(file)
+#         #
+#         # print(b1, b2, b3, b4, sep="\n")
+#
+#     finally:
+#         file.close()
+# except FileNotFoundError:
+#     print("Невозможно открыть файл")
 
 # _____________________________________________________
-
-
-
-
-
-
-
-
 
 
 
---
+++ Tutorials for Python/module_7/m_7_with.py
@@ -0,0 +1,58 @@
+file = open('languages.txt', 'r', encoding='utf-8')
+
+for line in file:
+    print(line)
+
+file.close()              # ручное закрытие файла
+
+print('Файл закрыт')
+
+# можно переписать в виде:
+with open('languages.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        print(line)
+                          # автоматическое закрытие файла
+print('Файл закрыт')
+
+#==========================================================
+
+try:
+    with open("my_file.txt") as fp:
+        for t in fp:
+            print(t)
+except Exception as e:
+    print(e)
+
+#==========================================================
+try:
+    with open("my_file.txt") as fin:
+        with open("out.txt", "w") as fout:
+            for line in fin:
+                fout.write(line)
+except Exception as e:
+    print(e)
+
+#==========================================================
+
+with open('philosophers.txt', 'w', encoding='utf-8') as output:
+    print('Джoн Локк', file=output)
+    print('Дэвид Хьюм', file=output)
+    print('Эдмyнд Берк', file=output)
+
+#==========================================================
+
+with open('philosophers.txt', 'w', encoding='utf-8') as output:
+    print('Джoн Локк', 'Дэвид Хьюм', 'Эдмyнд Берк', sep='***', file=output)
+
+
+
+
+
+
+
+
+
+
+
+
+

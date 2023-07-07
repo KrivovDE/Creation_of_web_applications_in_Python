54098a376dba2738f2e3e65a7374fd6e231bcf1a
Dmitrii Krivov <d.krivov@kitcup.ru>
2023-07-07 16:27:41 +0300

    peresort_3

--- Tutorials for JS/module_7_JSON_Ajax/mod_7_lesson_2_Ajax/socket_server.py
+++ Tutorials for JS/module_7_JSON_Ajax/mod_7_lesson_2_Ajax/socket_server.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind(('localhost', 3030))  # Привязываем серверный сокет к localhost и 3030 порту.
+s.bind(("localhost", 3030))  # Привязываем серверный сокет к localhost и 3030 порту.
 s.listen(1)  # Начинаем прослушивать входящие соединения.
 conn, addr = s.accept()  # Метод который принимает входящее соединение.
 
@@ -11,5 +11,5 @@ while True:
     if not data:
         break
     conn.sendall(data)  # Отправляем данные в сокет.
-    print(data.decode('utf-8'))
+    print(data.decode("utf-8"))
 conn.close()

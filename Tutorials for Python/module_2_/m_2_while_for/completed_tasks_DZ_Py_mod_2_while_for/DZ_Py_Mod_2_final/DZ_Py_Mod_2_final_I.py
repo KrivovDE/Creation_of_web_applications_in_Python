e952a699e2667b2d046583ead66713472e224d6e
Dmitrii Krivov <d.krivov@kitcup.ru>
2023-07-20 18:37:23 +0300

    peresort_20

--- Tutorials for Python/module_2_/m_2_while_for/completed_tasks_DZ_Py_mod_2_while_for/DZ_Py_Mod_2_final/DZ_Py_Mod_2_final_I.py
+++ Tutorials for Python/module_2_/m_2_while_for/completed_tasks_DZ_Py_mod_2_while_for/DZ_Py_Mod_2_final/DZ_Py_Mod_2_final_I.py
@@ -3,18 +3,18 @@
 # n, которые кратны или 3 или 5. Результат (сумму) вывести на экран.
 # Пример: n = 10, имеем числа: 3, 5, 6, 9. Их сумма равна 23.
 
-n = int(input('Число:\n--> '))
-digits = ''
+n = int(input("Число:\n--> "))
+digits = ""
 total = 0
 
 for i in range(1, n):
     if i % 3 == 0 or i % 5 == 0:
-        digits += str(i) + '; '
+        digits += str(i) + "; "
         total += i
 
-print(f'n = {n}\n'
-      f'Имеем числа: {digits}\n'
-      f'Их сумма равна {total}')
+print(
+    f"n = {n}\n" f"Имеем числа: {digits}\n" f"Их сумма равна {total}",
+)
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -29,23 +29,23 @@ print(f'n = {n}\n'
 # Если расставить машины согласно условию задачи невозможно,
 # выведите строку “Нет решения”.
 
-red_cars = int(input('Кол-во красных: '))
-white_cars = int(input('Кол-во белых: '))
+red_cars = int(input("Кол-во красных: "))
+white_cars = int(input("Кол-во белых: "))
 
-if (red_cars > 2*white_cars) or (white_cars > 2*red_cars):
-    print('Нет решения')
+if (red_cars > 2 * white_cars) or (white_cars > 2 * red_cars):
+    print("Нет решения")
 elif red_cars >= white_cars:
     diff_cars = red_cars - white_cars
     for _ in range(diff_cars):
-        print('RWR', end='')
+        print("RWR", end="")
     for _ in range(white_cars - diff_cars):
-        print('RW', end='')
+        print("RW", end="")
 else:
     diff_cars = white_cars - red_cars
     for _ in range(diff_cars):
-        print('WRW', end='')
+        print("WRW", end="")
     for _ in range(red_cars - diff_cars):
-        print('WR', end='')
+        print("WR", end="")
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TASK 3~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -56,10 +56,12 @@ else:
 try:
     from random import randint
 
-    print('Оценка на знание таблицы умножения.\n'
-    'Программа выводит на экран пример, пользователь должен ввести верный ответ.\n'
-    'С каждым пятым верным ответом сложноть будет увеличиваться, всего 5 уровней.\n'
-    'В любой момент можно выйти из программы и узнать результат написав в ответ 0.\n')
+    print(
+        "Оценка на знание таблицы умножения.\n"
+        "Программа выводит на экран пример, пользователь должен ввести верный ответ.\n"
+        "С каждым пятым верным ответом сложноть будет увеличиваться, всего 5 уровней.\n"
+        "В любой момент можно выйти из программы и узнать результат написав в ответ 0.\n",
+    )
 
     answer = 1
     lvl = 1
@@ -96,45 +98,50 @@ try:
         num_1 = randint(min1, max1)
         num_2 = randint(min2, max2)
 
-        answer = int(input(f'{num_1} * {num_2} = '))
+        answer = int(input(f"{num_1} * {num_2} = "))
 
         if answer == 0:
             break
-        elif answer == num_1*num_2:
-            print('Верно\n')
+        elif answer == num_1 * num_2:
+            print("Верно\n")
             total_y += 1
             total += 1
             if total_y % 5 == 0 and total_y != 0:
-                print('LVL UP!')
+                print("LVL UP!")
                 lvl += 1
-        elif answer != num_1*num_2:
-            print('Неверно\n'
-                  f'Правильный ответ: {num_1*num_2}\n')
+        elif answer != num_1 * num_2:
+            print(
+                "Неверно\n" f"Правильный ответ: {num_1*num_2}\n",
+            )
             total += 1
 
-    grade = ''
+    grade = ""
     score = 0
     if total != 0:
         score = total_y * 100 / total
 
     if score <= 20:
-        grade = '(Отсылка на мем)\nЧе закибербуйлили тебя, да?\n' \
-                'Ну я не знаю, выключи компьютер хз\n' \
-                'Ди отсюда давай'
+        grade = (
+            "(Отсылка на мем)\nЧе закибербуйлили тебя, да?\n"
+            "Ну я не знаю, выключи компьютер хз\n"
+            "Ди отсюда давай"
+        )
     elif 20 < score <= 40:
-        grade = 'Все ясно, двоечник'
+        grade = "Все ясно, двоечник"
     elif 40 < score <= 60:
-        grade = 'Харош'
+        grade = "Харош"
     elif 60 < score <= 80:
-        grade = 'МегаХарош'
+        grade = "МегаХарош"
     elif 80 < score:
-        grade = 'УльтраМегаХарош'
+        grade = "УльтраМегаХарош"
 
-    print(f'Уровень сложности - {lvl}\n'
-          f'Всего ответов: {total}\n'
-          f'Кол-во верных ответов - {total_y}\n'
-          f'Процентное соотношение вреных ответов - {round(score, 1)}\n'
-          f'Ваша оценка - {grade}')
+    print(
+        f"Уровень сложности - {lvl}\n"
+        f"Всего ответов: {total}\n"
+        f"Кол-во верных ответов - {total_y}\n"
+        f"Процентное соотношение вреных ответов - {round(score, 1)}\n"
+        f"Ваша оценка - {grade}",
+    )
 
 except ValueError as err:
-    print(f'Ops :(\nERROR: {err}')
+    print(f"Ops :(\nERROR: {err}")

1762849cfc20d7ec36481d2447ec0e6fd820b5b2
DKrivov <d.krivov@kitcup.ru>
2023-08-09 20:56:27 +0300

    peresort_41

--- Tutorials for Python/module_4_/m_4_l_4_gen.py
+++ Tutorials for Python/module_4_/m_4_l_4_gen.py
@@ -1,4 +1,3 @@
-
 # iter_1 = (x**2 for x in range(10))
 #
 # for i in iter_1:
@@ -14,6 +13,7 @@
 #         break
 # #---------------------------------------------------
 
+
 def _get_all_average(n):
     avs = []
     count = 0
@@ -24,28 +24,31 @@ def _get_all_average(n):
         avs.append(res / count)
     return avs
 
+
 print(_get_all_average(100000).__sizeof__())
+
 
 def get_all_average(n):
     count = 0
     res = 0
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         count += 1
         res += i
-        yield res/count
+        yield res / count
+
 
 it = get_all_average(100000)
 # print(next(it))
 # print(next(it))
 # print(next(it))
-print('generator', next(it).__sizeof__())
+print("generator", next(it).__sizeof__())
 
 
 #
 #
 # print(get_all_average(100))
 # print(get_all_average(100000).__sizeof__())
-#---------------------------------------------------
+# ---------------------------------------------------
 
 # def f():
 #     return list(range(10))
@@ -71,7 +74,7 @@ print('generator', next(it).__sizeof__())
 # lst_1 = list(i ** 2 for i in range(a, b + 1))
 #
 # print(lst_1)
-#---------------------------------------------------
+# ---------------------------------------------------
 
 # На вход программы поступают два целых числа a и b (a < b), записанные в одну строчку через пробел.
 # Определите генератор, который бы выдавал модули целых чисел из диапазона [a; b].
@@ -83,7 +86,7 @@ gen = (abs(x) for x in range(a, b + 1))
 for i in range(5):
     print(next(gen))
 
-#---------------------------------------------------
+# ---------------------------------------------------
 # Необходимо записать генератор, который бы используя этот список, выдавал 1 000 000 наименований городов
 # по циклу. То есть, дойдя до конца списка, возвращался в начало и повторял перебор. И так, для выдачи миллиона названий.
 # Вывести на экран первые 20 наименований городов с помощью генератора в одну строчку через пробел.
@@ -94,17 +97,8 @@ counter = 0
 for x in gen:
     if counter == 20:
         break
-    print(x, end=' ')
+    print(x, end=" ")
     counter += 1
-
-
-
-
-
-
-
-
-
 
 
 cities = ["Москва", "Ульяновск", "Самара", "Уфа", "Омск", "Тула"]
@@ -112,5 +106,3 @@ gen = (city for j in range(1000001) for city in cities)
 print(*list(gen)[:20])
 #
 #
-
-

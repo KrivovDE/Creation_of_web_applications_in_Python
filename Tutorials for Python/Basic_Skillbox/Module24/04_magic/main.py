c12e4532b82d6cdcfce1f830d7cf13cb48775c86
DKrivov <d.krivov@kitcup.ru>
2023-09-18 08:29:48 +0300

    base_skill_2

---
+++ Tutorials for Python/Basic_Skillbox/Module24/04_magic/main.py
@@ -0,0 +1,59 @@
+# TODO здесь писать код
+
+class Water:
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Ground):
+            return Dirt()
+        else:
+            raise ValueError('Сочетание невозможно.')
+class Air:
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Ground):
+            return Dust()
+        else:
+            raise ValueError('Сочетание невозможно.')
+class Fire:
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Lightning()
+        elif isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Ground):
+            return Lava()
+        else:
+            raise ValueError('Сочетание невозможно.')
+class Ground:
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Dust()
+        elif isinstance(other, Fire):
+            return Lava()
+        elif isinstance(other, Water):
+            return Dirt()
+        else:
+            raise ValueError('Сочетание невозможно.')
+
+class Storm:
+    pass
+class Steam:
+    pass
+class Dirt:
+    pass
+class Lightning:
+    pass
+class Dust:
+    pass
+class Lava:
+    pass
+
+print(type(Air() + Water()))
+print(type(Ground() + Fire()))

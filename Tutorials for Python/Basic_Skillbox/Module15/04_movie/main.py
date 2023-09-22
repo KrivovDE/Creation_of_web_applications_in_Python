52edeaaca3e480f6fce2485571c01586f3a81f50
DKrivov <d.krivov@kitcup.ru>
2023-09-22 08:30:58 +0300

    base_skill_6

---
+++ Tutorials for Python/Basic_Skillbox/Module15/04_movie/main.py
@@ -0,0 +1,36 @@
+films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
+         'Леон', 'Богемская рапсодия', 'Город грехов',
+         'Мементо', 'Отступники', 'Деревня']
+
+
+# TODO здесь писать код
+
+
+def film_search(request):
+    for film in films:
+        if request == film:
+            return True
+    else:
+        return False
+
+
+favorite_films = []
+counter = 0
+numbers_films = int(input('Сколько фильмов хотите добавить? '))
+
+for _ in range(numbers_films):
+    film = input('Введите название фильма: ')
+    if film_search(film):
+        favorite_films.append(film)
+    else:
+        print(f'Ошибка: фильма {film} у нас нет :(')
+counter = len(favorite_films)
+
+print('Ваш список любимых фильмов:', end=' ')
+for name in favorite_films:
+    print(name, end='')
+    if counter - 1 > 0:
+        print(',', end=' ')
+        counter -= 1
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module26/01_num_squares/main.py
@@ -0,0 +1,48 @@
+# TODO здесь писать код
+
+
+class NumSquares:
+
+    def __init__(self, max_num: int):
+        self.max_num = max_num
+        self.num = 0
+
+    def __iter__(self):
+        self.num = 0
+        return self
+
+    def __next__(self) -> int:
+        self.num += 1
+        if self.num <= self.max_num:
+            return self.num ** 2
+        else:
+            raise StopIteration
+
+
+def num_squares(max_num: int):
+    for i_num in range(1, max_num + 1):
+        yield i_num ** 2
+    else:
+        return
+
+
+max_num = int(input('Введите число: '))
+
+squares_gen = (i_num ** 2 for i_num in range(1, max_num + 1))
+ver_1 = NumSquares(max_num)
+
+print('Класс-итератор -', end=' ')
+for i_num in ver_1:
+    print(i_num, end=' ')
+print()
+
+print('Функция-генератор -', end=' ')
+for i_num in num_squares(max_num):
+    print(i_num, end=' ')
+print()
+
+print('Генераторное выражение -', end=' ')
+for i_num in squares_gen:
+    print(i_num, end=' ')
+
+# зачтено

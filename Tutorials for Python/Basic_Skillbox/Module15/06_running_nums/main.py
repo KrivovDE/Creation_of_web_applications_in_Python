fb9f08c34c6785fabca982314bbe52beadfd70f4
DKrivov <d.krivov@kitcup.ru>
2023-09-28 10:18:08 +0300

    base_skill_16

---
+++ Tutorials for Python/Basic_Skillbox/Module15/06_running_nums/main.py
@@ -0,0 +1,20 @@
+# TODO здесь писать код
+
+
+original_list = []
+shifted_list = []
+sequence_size = int(input('Введите размер последовательности: '))
+
+for _ in range(sequence_size):
+    ver = int(input('Введите число: '))
+    original_list.append(ver)
+
+shift = int(input('Сдвиг: '))
+
+for i in range(0 - shift, len(original_list) - shift, 1):
+    shifted_list.append(original_list[i])
+
+print('Изначальный список:', original_list)
+print('Сдвинутый список:', shifted_list)
+
+# зачтено

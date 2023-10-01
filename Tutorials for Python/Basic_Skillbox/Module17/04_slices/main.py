b5559764df761c7fb5b632717de293f8809d9702
DKrivov <d.krivov@kitcup.ru>
2023-10-01 11:48:38 +0300

    base_skill_20

---
+++ Tutorials for Python/Basic_Skillbox/Module17/04_slices/main.py
@@ -0,0 +1,18 @@
+alphabet = 'abcdefg'
+
+# TODO здесь писать код
+
+alphabet_copy = alphabet[:]
+
+print(alphabet_copy)
+print(alphabet_copy[:: -1])
+print(alphabet_copy[:: 2])
+print(alphabet_copy[1:: 2])
+print(alphabet_copy[: 1])
+print(alphabet_copy[len(alphabet_copy): len(alphabet_copy) - 2: -1])
+print(alphabet_copy[3: 4])
+print(alphabet_copy[len(alphabet_copy) - 3: len(alphabet_copy)])
+print(alphabet_copy[3: 4 + 1])
+print(alphabet_copy[4: 3 - 1: -1])
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module17/06_two_dimensional_list/main.py
@@ -0,0 +1,5 @@
+# TODO здесь писать код
+
+print([list(range(num, num + 10, 4)) for num in range(1, 5)])
+
+# зачтено

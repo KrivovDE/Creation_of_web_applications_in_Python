2f45d83bb3bca39b41e23c579d4e941d3b0af63a
DKrivov <d.krivov@kitcup.ru>
2023-10-02 08:26:45 +0300

    base_skill_20

---
+++ Tutorials for Python/Basic_Skillbox/Module16/01_scary_code/main.py
@@ -0,0 +1,17 @@
+main_list = [1, 5, 3]
+add_list_1 = [1, 5, 1, 5]
+add_list_2 = [1, 3, 1, 5, 3, 3]
+
+# TODO переписать программу
+
+main_list.extend(add_list_1)
+print('Кол-во цифр 5 при первом объединении:', main_list.count(5))
+
+for _ in range(main_list.count(5)):
+    main_list.remove(5)
+
+main_list.extend(add_list_2)
+print('Кол-во цифр 3 при втором объединении:', main_list.count(3))
+print('Итоговый список:', main_list)
+
+# зачтено

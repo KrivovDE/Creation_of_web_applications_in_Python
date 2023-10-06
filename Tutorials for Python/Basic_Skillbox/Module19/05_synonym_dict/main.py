964a8dacdd6493043b0e5eb172d859c50d6d2fcd
DKrivov <d.krivov@kitcup.ru>
2023-10-06 10:57:39 +0300

    base_skill_25

---
+++ Tutorials for Python/Basic_Skillbox/Module19/05_synonym_dict/main.py
@@ -0,0 +1,31 @@
+# TODO здесь писать код
+
+
+def value_search(word, input_dict):
+    for key, value in input_dict.items():
+        if value == word:
+            return key
+    else:
+        return False
+
+
+num_pair = int(input('Введите количество пар слов: '))
+syn_dict = {}
+
+for i_num in range(num_pair):
+    print(f'{i_num + 1}-я пара:', end=' ')
+    new_word = input().split(' — ')
+    syn_dict[new_word[0].capitalize()] = new_word[1].capitalize()
+
+while True:
+    searched_wd = input('Введите слово: ').capitalize()
+    if searched_wd in syn_dict:
+        print('Синоним:', syn_dict[searched_wd])
+        break
+    elif value_search(searched_wd, syn_dict):
+        print('Синоним:', value_search(searched_wd, syn_dict))
+        break
+    else:
+        print('Такого слова в словаре нет.')
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module20/03_players/main.py
@@ -0,0 +1,11 @@
+players = {
+    ("Ivan", "Volkin"): (10, 5, 13),
+    ("Bob", "Robbin"): (7, 5, 14),
+    ("Rob", "Bobbin"): (12, 8, 2)
+}
+
+# TODO здесь писать код
+
+print([(name[0], name[1], score[0], score[1], score[2]) for name, score in players.items()])
+
+# зачтено

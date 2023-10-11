294cebef2140c5e2c54dd3b3de2d4cb71a83fe5b
DKrivov <d.krivov@kitcup.ru>
2023-10-11 11:38:48 +0300

    base_skill_32

---
+++ Tutorials for Python/Basic_Skillbox/Module18/08_ticker/main.py
@@ -0,0 +1,20 @@
+# TODO здесь писать код
+
+
+user_str_1 = input('Первая строка: ')
+user_str_2 = input('Вторая строка: ')
+temp_str = list(user_str_2)
+
+if user_str_1 == user_str_2:
+    print('Введённые строки идентичны.')
+else:
+    for counter in range(len(user_str_2) - 1):
+        temp_str.insert(0, temp_str[-1])
+        del temp_str[-1]
+        if user_str_1 == ''.join(temp_str):
+            print('Первая строка получается из второй со сдвигом {}.'.format(counter + 1))
+            break
+    else:
+        print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
+
+# зачтено

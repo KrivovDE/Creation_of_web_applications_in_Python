a6a7795fe99f37e26d576f200cec163cd855a3ab
DKrivov <d.krivov@kitcup.ru>
2023-10-11 23:50:09 +0300

    base_skill_32

---
+++ Tutorials for Python/Basic_Skillbox/Module17/02_generation/main.py
@@ -0,0 +1,6 @@
+# TODO здесь писать код
+
+user_num = int(input('Введите длину списка: '))
+print('Результат:', [(1 if num % 2 == 0 else num % 5) for num in range(user_num)])
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module17/07_list_of_lists_2/main.py
@@ -0,0 +1,15 @@
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+             [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+
+# TODO здесь писать код
+
+print([level_1 for level_3 in nice_list for level_2 in level_3 for level_1 in level_2])
+
+# new_list = []
+# for level_3 in nice_list:
+#     for level_2 in level_3:
+#         for level_1 in level_2:
+#             new_list.append(level_1)
+# print(new_list)
+
+# зачтено

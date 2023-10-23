1eb4f2719a5b7160d1059dd9e4c52ddd72c25f3d
DKrivov <d.krivov@kitcup.ru>
2023-10-23 12:35:17 +0300

    base_skill_48

---
+++ Tutorials for Python/Basic_Skillbox/Module30/01_new_lists/main.py
@@ -0,0 +1,17 @@
+# TODO здесь писать код
+
+
+from functools import reduce
+from typing import List
+
+floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
+names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
+numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
+
+cubes_floats = list(map(lambda x: round(x ** 3, 3), floats))
+longest_names = list(filter(lambda x: len(x) >= 5, names))
+multiplex = reduce(lambda x, y: x * y, numbers)
+
+print(cubes_floats, longest_names, multiplex, sep='\n')
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module30/02_zip_again/main.py
@@ -0,0 +1,12 @@
+# TODO здесь писать код
+
+from typing import List
+
+letters: List[str] = ['a', 'b', 'c', 'd', 'e']
+numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+results = list(map(lambda x, y: (x, y), letters, numbers))
+
+print(results)
+
+# зачтено

b787438fb63d36d64fab93c96dd10f3bf96d4e6e
DKrivov <d.krivov@kitcup.ru>
2023-10-23 08:40:42 +0300

    base_skill_46

---
+++ Tutorials for Python/Basic_Skillbox/Module18/05_password/main.py
@@ -0,0 +1,24 @@
+# TODO здесь писать код
+
+def complexity_check(ps_wd):
+    cointers = [len(ps_wd), 0, 0]
+    for sign in ps_wd:
+        if sign.isdigit():
+            cointers[1] += 1
+        elif sign.isupper():
+            cointers[2] += 1
+    if cointers[0] < 8 or cointers[1] < 3 or cointers[2] < 1:
+        return False
+    else:
+        return True
+
+
+while True:
+    password = input('Придумайте пароль: ')
+    if complexity_check(password):
+        print('Это надёжный пароль!')
+        break
+    else:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module18/06_compression/main.py
@@ -0,0 +1,21 @@
+# TODO здесь писать код
+
+
+user_str = input('Введите строку: ')
+encrypted_list = []
+counters = [user_str[0], 0]
+
+for sign in user_str:
+    if sign == counters[0]:
+        counters[1] += 1
+    else:
+        encrypted_list.append(counters.copy())
+        counters[0] = sign
+        counters[1] = 1
+encrypted_list.append(counters.copy())
+
+encrypted_str = [str(ver) for level_2 in encrypted_list for ver in level_2]
+
+print(''.join(encrypted_str))
+
+# зачтено

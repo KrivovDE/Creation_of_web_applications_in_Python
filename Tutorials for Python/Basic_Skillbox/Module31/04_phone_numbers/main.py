e1c5f4e654bd9d6800f94340186136af65ca00cc
DKrivov <d.krivov@kitcup.ru>
2023-10-24 19:10:08 +0300

    base_skill_49

---
+++ Tutorials for Python/Basic_Skillbox/Module31/04_phone_numbers/main.py
@@ -0,0 +1,11 @@
+# TODO здесь писать код
+
+import re
+
+text = ['9999999999', '999999-999', '99999x9999']
+
+for i_text in text:
+    if re.fullmatch(r'[89]\d{9}', i_text) is None:
+        print(i_text, '- не подходит')
+    else:
+        print(i_text, '- всё в порядке')

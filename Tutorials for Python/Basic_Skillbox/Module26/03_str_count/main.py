ef260ff05f884b2fc0e61a92a495b98061629cbb
DKrivov <d.krivov@kitcup.ru>
2023-10-25 13:47:27 +0300

    base_skill_50

---
+++ Tutorials for Python/Basic_Skillbox/Module26/03_str_count/main.py
@@ -0,0 +1,20 @@
+# TODO здесь писать код
+import os
+
+
+def str_counter_gen():
+    for i_file in os.listdir():
+        if '.py' in i_file:
+            counter = 0
+            with open(i_file, 'r', encoding='utf-8') as file:
+                for i_str in file:
+                    if not (len(i_str) < 2 or '"""' in i_str or '#' in i_str):
+                        counter += 1
+                else:
+                    yield counter
+
+
+for i_sum in str_counter_gen():
+    print(i_sum)
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module28/02_math_module/main.py
@@ -0,0 +1,35 @@
+# TODO здесь писать код
+
+from abc import ABC
+
+class MyMath(ABC):
+
+    """Абстрактный класс математических операций"""
+
+    _Pi = 3.1415926535
+
+    @classmethod
+    def circle_len(cls, radius: float) -> float:
+        """Возвращает длину окружности исходя из входного радиуса"""
+        return 2 * cls._Pi*radius
+
+    @classmethod
+    def circle_sq(cls, radius: float) -> float:
+        """Возвращает площадь окружности исходя из входного радиуса"""
+        return 2 * cls._Pi * radius ** 2
+
+    @classmethod
+    def cub_vol(cls, fin_length: float) -> float:
+        """Возвращает объём куда исходя из длинны ребра"""
+        return fin_length ** 3
+
+    @classmethod
+    def surface_area_sphere(cls, radius: float) -> float:
+        """Возвращает площадь поверхности сферы исходя из входного радиуса"""
+        return 2 * cls._Pi*radius ** 2
+
+
+res_1 = MyMath.circle_len(radius=5)
+res_2 = MyMath.circle_sq(radius=6)
+print(res_1)
+print(res_2)

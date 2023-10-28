373e7964628f41a0054a5b28e0dc7e7815ce9235
DKrivov <d.krivov@kitcup.ru>
2023-10-28 18:34:09 +0300

    base_skill_53

---
+++ Tutorials for Python/Basic_Skillbox/Module30/03_palindrome_the_return/main.py
@@ -0,0 +1,20 @@
+# TODO здесь писать код
+
+from collections import deque
+
+
+def can_be_poly(data: str) -> bool:
+    deque_str = deque(data)
+
+    while len(deque_str) > 1:
+        first_sign = deque_str.pop()
+        last_sign = deque_str.popleft()
+        if first_sign != last_sign:
+            return False
+    return True
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module30/04_unique_cipher/main.py
@@ -0,0 +1,16 @@
+# TODO здесь писать код
+
+from collections import Counter
+
+
+def count_unique_characters(mess: str) -> int:
+    return len(list(filter(lambda y: y[1] == 1, map(lambda x: x, Counter(mess.lower()).items()))))
+
+
+# Пример использования:
+message = "Today is a beautiful day! The sun is shining and the birds are singing."
+
+unique_count = count_unique_characters(message)
+print("Количество уникальных символов в строке:", unique_count)
+
+# зачтено

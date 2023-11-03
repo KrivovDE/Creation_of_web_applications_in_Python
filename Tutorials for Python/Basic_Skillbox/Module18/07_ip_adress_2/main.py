15998b091668b5dd13dd7e739f59fa363f2316de
DKrivov <d.krivov@kitcup.ru>
2023-11-03 08:12:23 +0300

    base_skill_61

---
+++ Tutorials for Python/Basic_Skillbox/Module18/07_ip_adress_2/main.py
@@ -0,0 +1,26 @@
+# TODO здесь писать код
+
+def num_check(ip_list):
+    for num in ip_list:
+        if not (num.isdigit()):
+            return num
+        elif not (0 <= int(num) <= 255):
+            return num
+    else:
+        return False
+
+
+ip_address = input('Введите IP: ')
+num_list = ip_address.split('.')
+result_check = num_check(num_list)
+
+if not result_check:
+    print('IP-адрес корректен.')
+elif ip_address.count('.') < 3:
+    print('Адрес — это четыре числа, разделённые точками.')
+elif not (result_check.isdigit()):
+    print(result_check, '— это не целое число.')
+elif result_check:
+    print(f'{result_check} выходит за допустимый диапазон [0 255]')
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module20/04_pairs/main.py
@@ -0,0 +1,25 @@
+# TODO здесь писать код
+
+import random
+
+
+def split_by_two(input_list):
+    prev_value = 0
+    for index, value in enumerate(input_list):
+        if index % 2 != 1:
+            prev_value = value
+        else:
+            new_list_2.append((prev_value, value))
+
+
+orig_list = [random.randint(0, 100) for _ in range(10)]
+new_list = [(orig_list[i_num], orig_list[i_num + 1]) for i_num in range(0, len(orig_list), 2)]
+new_list_2 = []
+
+split_by_two(orig_list)
+
+print(orig_list)
+print(new_list)
+print(new_list_2)
+
+# зачтено

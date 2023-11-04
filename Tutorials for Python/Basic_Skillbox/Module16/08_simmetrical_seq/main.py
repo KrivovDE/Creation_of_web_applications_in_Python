d55773f546b4854385d2272154cabc91efd6e569
DKrivov <d.krivov@kitcup.ru>
2023-11-05 00:41:10 +0300

    base_skill_63

---
+++ Tutorials for Python/Basic_Skillbox/Module16/08_simmetrical_seq/main.py
@@ -0,0 +1,33 @@
+# TODO здесь писать код
+
+def symmetry_check(list_num):
+    limit = int(len(list_num) / 2) if len(list_num) % 2 == 0 else int((len(list_num) - 1) / 2)
+    for i_num in range(limit):
+        if list_num[i_num] != list_num[-i_num - 1]:
+            return False
+    else:
+        return True
+
+
+sequence_size = int(input('Кол-во чисел: '))
+input_seq = []
+for _ in range(sequence_size):
+    input_seq.append(int(input('Число: ')))
+
+# input_seq = [1, 2, 1, 2, 2, 1, 2, 1]
+add = []
+counter = 0
+
+print('\nПоследовательность:', input_seq)
+if symmetry_check(input_seq):
+    print('Введённая последовательность симметрична!')
+else:
+    temp_seq = input_seq
+    while not (symmetry_check(temp_seq)):
+        add.insert(0, temp_seq[0])
+        temp_seq.remove(temp_seq[0])
+        counter += 1
+    print('Нужно приписать чисел:', counter)
+    print('Сами числа:', add)
+
+# зачтено
---
+++ Tutorials for Python/Basic_Skillbox/Module17/08_caesar_cipher/main.py
@@ -0,0 +1,30 @@
+# TODO здесь писать код
+def is_letter(letter):
+    for sign in alphabet:
+        if letter == sign:
+            return True
+    else:
+        return False
+
+
+def index_search(letter, shift):
+    required_index = alphabet.index(letter) + shift if alphabet.index(letter) + shift < len(
+        alphabet) else alphabet.index(letter) + shift - ((alphabet.index(letter) + shift) // len(alphabet)) * len(
+        alphabet)
+    return alphabet[required_index]
+
+
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+user_text = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
+encrypted_text = ''
+
+for letter in user_text:
+    if is_letter(letter):
+        encrypted_text += index_search(letter, shift)
+    else:
+        encrypted_text += letter
+
+print('Зашифрованное сообщение:', encrypted_text)
+
+# зачтено

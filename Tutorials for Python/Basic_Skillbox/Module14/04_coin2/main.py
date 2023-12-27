89345ddd63aa99fd8112f4db671ea64228364635
DKrivov <d.krivov@kitcup.ru>
2023-12-27 10:44:33 +0300

    base_skill_117

--- Tutorials for Python/Basic_Skillbox/Module14/04_coin2/main.py
+++ Tutorials for Python/Basic_Skillbox/Module14/04_coin2/main.py
@@ -5,16 +5,16 @@ import math
 
 
 def point_check(x, y, r):
-    return True if round(math.sqrt(x ** 2 + y ** 2), 2) <= round(r, 2) else False
+    return True if round(math.sqrt(x**2 + y**2), 2) <= round(r, 2) else False
 
 
-abscissa = float(input('Введите координаты монетки:\nX: '))
-ordinate = float(input('Y: '))
-radius = float(input('Введите радиус: '))
+abscissa = float(input("Введите координаты монетки:\nX: "))
+ordinate = float(input("Y: "))
+radius = float(input("Введите радиус: "))
 
 if point_check(abscissa, ordinate, radius):
-    print('Монетка где-то рядом')
+    print("Монетка где-то рядом")
 else:
-    print('Монетки в области нет')
+    print("Монетки в области нет")
 
 # зачтено

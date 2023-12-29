d5f192f0a0280f4fb1e282e6cfccc18fcd81807e
DKrivov <d.krivov@kitcup.ru>
2023-12-29 11:06:20 +0300

    base_skill_119

--- Tutorials for Python/Basic_Skillbox/Module15/02_tournament/main.py
+++ Tutorials for Python/Basic_Skillbox/Module15/02_tournament/main.py
@@ -1,11 +1,11 @@
 # TODO здесь писать код
 
-list_of_names = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+list_of_names = ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
 new_list = []
 
 for i in range(0, len(list_of_names), 2):
     new_list.append(list_of_names[i])
 
-print('Первый день:', new_list)
+print("Первый день:", new_list)
 
 # зачтено
--- Tutorials for Python/Basic_Skillbox/Module15/03_videocards/main.py
+++ Tutorials for Python/Basic_Skillbox/Module15/03_videocards/main.py
@@ -4,10 +4,10 @@ card_list = []
 remaining_card = []
 top_card = 0
 
-numbers_card = int(input('Количество видеокарт: '))
+numbers_card = int(input("Количество видеокарт: "))
 
 for i in range(numbers_card):
-    card = int(input(f'{i + 1} Видеокарта: '))
+    card = int(input(f"{i + 1} Видеокарта: "))
     top_card = card if card > top_card else top_card
     card_list.append(card)
 
@@ -15,7 +15,7 @@ for ver in card_list:
     if ver != top_card:
         remaining_card.append(ver)
 
-print('Старый список видеокарт:', card_list)
-print('Новый список видеокарт:', remaining_card)
+print("Старый список видеокарт:", card_list)
+print("Новый список видеокарт:", remaining_card)
 
 # зачтено

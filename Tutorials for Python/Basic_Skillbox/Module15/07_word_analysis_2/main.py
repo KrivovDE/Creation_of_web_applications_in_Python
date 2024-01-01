992ab53a36763abdf742c3eb295dd5fb3487b396
DKrivov <d.krivov@kitcup.ru>
2024-01-01 10:49:51 +0300

    1

--- Tutorials for Python/Basic_Skillbox/Module15/07_word_analysis_2/main.py
+++ Tutorials for Python/Basic_Skillbox/Module15/07_word_analysis_2/main.py
@@ -1,16 +1,16 @@
 # TODO здесь писать код
 
 
-reversed_word = ''
+reversed_word = ""
 
-word = input('Введите слово: ')
+word = input("Введите слово: ")
 
 for i in range(-1, -(len(word) + 1), -1):
     reversed_word += word[i]
 
 if word == reversed_word:
-    print('Слово является палиндромом')
+    print("Слово является палиндромом")
 else:
-    print('Слово не является палиндромом')
+    print("Слово не является палиндромом")
 
 # зачтено

a51e446fb0c28fa39d0bdebb8df1f6479c3deadf
DKrivov <d.krivov@kitcup.ru>
2024-01-02 21:00:25 +0300

    2

--- Tutorials for Python/Basic_Skillbox/Module16/01_scary_code/main.py
+++ Tutorials for Python/Basic_Skillbox/Module16/01_scary_code/main.py
@@ -5,13 +5,13 @@ add_list_2 = [1, 3, 1, 5, 3, 3]
 # TODO переписать программу
 
 main_list.extend(add_list_1)
-print('Кол-во цифр 5 при первом объединении:', main_list.count(5))
+print("Кол-во цифр 5 при первом объединении:", main_list.count(5))
 
 for _ in range(main_list.count(5)):
     main_list.remove(5)
 
 main_list.extend(add_list_2)
-print('Кол-во цифр 3 при втором объединении:', main_list.count(3))
-print('Итоговый список:', main_list)
+print("Кол-во цифр 3 при втором объединении:", main_list.count(3))
+print("Итоговый список:", main_list)
 
 # зачтено

767765acae79ef83aba073525227bd407397a074
DKrivov <d.krivov@kitcup.ru>
2024-01-03 18:49:36 +0300

    3

--- Tutorials for Python/Basic_Skillbox/Module16/04_party/main.py
+++ Tutorials for Python/Basic_Skillbox/Module16/04_party/main.py
@@ -1,29 +1,29 @@
-guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+guests = ["Петя", "Ваня", "Саша", "Лиза", "Катя"]
 
 # TODO здесь писать код
 
 
-answer = ''
+answer = ""
 
 while True:
-    print(f'Сейчас на вечеринке {len(guests)} человек:', guests)
-    answer = input('Гость пришёл или ушёл? ')
-    if answer == 'пришёл':
-        name = input('Имя гостя: ')
+    print(f"Сейчас на вечеринке {len(guests)} человек:", guests)
+    answer = input("Гость пришёл или ушёл? ")
+    if answer == "пришёл":
+        name = input("Имя гостя: ")
         if len(guests) < 6:
-            print(f'Привет, {name}!')
+            print(f"Привет, {name}!")
             guests.append(name)
         else:
-            print(f'Прости, {name}, но мест нет.')
-    elif answer == 'ушёл':
-        name = input('Имя гостя: ')
+            print(f"Прости, {name}, но мест нет.")
+    elif answer == "ушёл":
+        name = input("Имя гостя: ")
         guests.remove(name)
-        print(f'Пока, {name}!')
-    elif answer == 'Пора спать':
-        print('Вечеринка закончилась, все легли спать.')
+        print(f"Пока, {name}!")
+    elif answer == "Пора спать":
+        print("Вечеринка закончилась, все легли спать.")
         break
     else:
-        print('A? Что?')
+        print("A? Что?")
     print()
 
 # зачтено

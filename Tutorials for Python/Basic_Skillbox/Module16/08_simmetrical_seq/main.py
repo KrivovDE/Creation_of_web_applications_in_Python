0c717e5825a09c9ef2ce9b61687659eef08c0a3b
DKrivov <d.krivov@kitcup.ru>
2024-01-06 20:29:01 +0300

    6

--- Tutorials for Python/Basic_Skillbox/Module16/08_simmetrical_seq/main.py
+++ Tutorials for Python/Basic_Skillbox/Module16/08_simmetrical_seq/main.py
@@ -1,7 +1,12 @@
 # TODO здесь писать код
 
+
 def symmetry_check(list_num):
-    limit = int(len(list_num) / 2) if len(list_num) % 2 == 0 else int((len(list_num) - 1) / 2)
+    limit = (
+        int(len(list_num) / 2)
+        if len(list_num) % 2 == 0
+        else int((len(list_num) - 1) / 2)
+    )
     for i_num in range(limit):
         if list_num[i_num] != list_num[-i_num - 1]:
             return False
@@ -9,25 +14,25 @@ def symmetry_check(list_num):
         return True
 
 
-sequence_size = int(input('Кол-во чисел: '))
+sequence_size = int(input("Кол-во чисел: "))
 input_seq = []
 for _ in range(sequence_size):
-    input_seq.append(int(input('Число: ')))
+    input_seq.append(int(input("Число: ")))
 
 # input_seq = [1, 2, 1, 2, 2, 1, 2, 1]
 add = []
 counter = 0
 
-print('\nПоследовательность:', input_seq)
+print("\nПоследовательность:", input_seq)
 if symmetry_check(input_seq):
-    print('Введённая последовательность симметрична!')
+    print("Введённая последовательность симметрична!")
 else:
     temp_seq = input_seq
     while not (symmetry_check(temp_seq)):
         add.insert(0, temp_seq[0])
         temp_seq.remove(temp_seq[0])
         counter += 1
-    print('Нужно приписать чисел:', counter)
-    print('Сами числа:', add)
+    print("Нужно приписать чисел:", counter)
+    print("Сами числа:", add)
 
 # зачтено
--- Tutorials for Python/Basic_Skillbox/Module17/01_vowels/main.py
+++ Tutorials for Python/Basic_Skillbox/Module17/01_vowels/main.py
@@ -1,7 +1,8 @@
 # TODO здесь писать код
 
+
 def vowel_check(check_sign):
-    vowel_list = 'ауоыиэяюёе'
+    vowel_list = "ауоыиэяюёе"
     for letter in vowel_list:
         if letter == check_sign:
             return True
@@ -9,10 +10,10 @@ def vowel_check(check_sign):
         return False
 
 
-original_text = list(input('Введите текст: '))
+original_text = list(input("Введите текст: "))
 filtred_text = [sign for sign in original_text if vowel_check(sign)]
 
-print('\nСписок гласных букв:', filtred_text)
-print('Длина списка:', len(filtred_text))
+print("\nСписок гласных букв:", filtred_text)
+print("Длина списка:", len(filtred_text))
 
 # зачтено

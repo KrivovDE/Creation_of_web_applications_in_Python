fb16a6f8472a4ef9ae2dbf732ba27c4ef2f3f915
DKrivov <d.krivov@kitcup.ru>
2024-01-08 09:42:28 +0300

    8

--- Tutorials for Python/Basic_Skillbox/Module17/03_random_competition/main.py
+++ Tutorials for Python/Basic_Skillbox/Module17/03_random_competition/main.py
@@ -4,10 +4,12 @@ import random
 
 team_1 = [round(random.randrange(5, 10) + random.random(), 2) for _ in range(20)]
 team_2 = [round(random.randrange(5, 10) + random.random(), 2) for _ in range(20)]
-winners_list = [team_1[num] if team_1[num] > team_2[num] else team_2[num] for num in range(20)]
+winners_list = [
+    team_1[num] if team_1[num] > team_2[num] else team_2[num] for num in range(20)
+]
 
-print('Первая команда:', team_1)
-print('Вторая команда:', team_2)
-print('Победители тура:', winners_list)
+print("Первая команда:", team_1)
+print("Вторая команда:", team_2)
+print("Победители тура:", winners_list)
 
 # зачтено

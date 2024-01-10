e44d13dff1bf408e8df49e384d360bdc936113ec
DKrivov <d.krivov@kitcup.ru>
2024-01-10 09:56:15 +0300

    10

--- Tutorials for Python/Basic_Skillbox/Module17/05_reversal/main.py
+++ Tutorials for Python/Basic_Skillbox/Module17/05_reversal/main.py
@@ -1,10 +1,11 @@
 # TODO здесь писать код
 
+
 def h_search(user_str):
     first_h, last_h = None, None
     flag = True
     for index_letter in range(len(user_str)):
-        if user_str[index_letter] == 'h':
+        if user_str[index_letter] == "h":
             if flag:
                 first_h = index_letter
                 flag = False
@@ -12,8 +13,11 @@ def h_search(user_str):
     return first_h + 1, last_h - 1
 
 
-original_str = input('Введите строку: ')
+original_str = input("Введите строку: ")
 start, finish = h_search(original_str)
-print('Развёрнутая последовательность между первым и последним h:', original_str[finish: start - 1: -1])
+print(
+    "Развёрнутая последовательность между первым и последним h:",
+    original_str[finish : start - 1 : -1],
+)
 
 # зачтено

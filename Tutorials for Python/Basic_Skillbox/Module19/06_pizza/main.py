103679541ddeda47ad92a8e812f144738ef4557f
DKrivov <d.krivov@kitcup.ru>
2024-01-27 04:38:29 +0600

    27

--- Tutorials for Python/Basic_Skillbox/Module19/06_pizza/main.py
+++ Tutorials for Python/Basic_Skillbox/Module19/06_pizza/main.py
@@ -1,11 +1,11 @@
 # TODO здесь писать код
 
-num_orders = int(input('Введите количество заказов: '))
+num_orders = int(input("Введите количество заказов: "))
 client_dict = {}
 
 for i_num in range(num_orders):
-    print(f'{i_num + 1}-й заказ:', end=' ')
-    user_order = input().split(' ')
+    print(f"{i_num + 1}-й заказ:", end=" ")
+    user_order = input().split(" ")
     if user_order[0] in client_dict:
         if user_order[1] in client_dict[user_order[0]]:
             client_dict[user_order[0]][user_order[1]] += int(user_order[2])
@@ -16,8 +16,8 @@ for i_num in range(num_orders):
 
 print()
 for client in sorted(client_dict.keys()):
-    print(f'{client}:')
+    print(f"{client}:")
     for pizza in sorted(client_dict[client].keys()):
-        print('    {0}: {1}'.format(pizza, client_dict[client][pizza]))
+        print(f"    {pizza}: {client_dict[client][pizza]}")
 
 # зачтено

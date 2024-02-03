37e24472c78ae9eb573d47815432a1f9efc7cf1f
DKrivov <d.krivov@kitcup.ru>
2024-02-03 11:51:32 +0300

    0203

--- Tutorials for Python/Basic_Skillbox/Module20/07_my_zip/main.py
+++ Tutorials for Python/Basic_Skillbox/Module20/07_my_zip/main.py
@@ -1,17 +1,18 @@
 # TODO здесь писать код
 
 
-sign_str = 'abcd'
+sign_str = "abcd"
 num_tuple = (10, 20, 30, 40)
 
-print('Строка:', sign_str)
-print('Кортеж чисел:', num_tuple)
+print("Строка:", sign_str)
+print("Кортеж чисел:", num_tuple)
 smaller_length = len(sign_str) if len(sign_str) < len(num_tuple) else len(num_tuple)
 
-pair_generator = ((sign_str[i_index], num_tuple[i_index])
-                  for i_index in range(smaller_length))
+pair_generator = (
+    (sign_str[i_index], num_tuple[i_index]) for i_index in range(smaller_length)
+)
 
-print('\nРезультат:\n', pair_generator)
+print("\nРезультат:\n", pair_generator)
 
 for i_pair in pair_generator:
     print(i_pair)

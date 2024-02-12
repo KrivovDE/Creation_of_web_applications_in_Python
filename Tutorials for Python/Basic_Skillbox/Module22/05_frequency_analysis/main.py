83f6b310f45d0af28a18b5a2e20b1c33f1fd5a0e
DKrivov <d.krivov@kitcup.ru>
2024-02-12 08:21:56 +0300

    0211

--- Tutorials for Python/Basic_Skillbox/Module22/05_frequency_analysis/main.py
+++ Tutorials for Python/Basic_Skillbox/Module22/05_frequency_analysis/main.py
@@ -14,9 +14,36 @@ letter_counter = 0
 letter_dict = {}
 sorted_list = []
 
-alphabet_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
+alphabet_dict = {
+    "a": 0,
+    "b": 1,
+    "c": 2,
+    "d": 3,
+    "e": 4,
+    "f": 5,
+    "g": 6,
+    "h": 7,
+    "i": 8,
+    "j": 9,
+    "k": 10,
+    "l": 11,
+    "m": 12,
+    "n": 13,
+    "o": 14,
+    "p": 15,
+    "q": 16,
+    "r": 17,
+    "s": 18,
+    "t": 19,
+    "u": 20,
+    "v": 21,
+    "w": 22,
+    "x": 23,
+    "y": 24,
+    "z": 25,
+}
 
-data_in = open('text.txt', 'r')
+data_in = open("text.txt")
 
 for i_str in data_in:
     for i_sign in i_str.lower():
@@ -36,18 +63,24 @@ for i_key, i_value in letter_dict.items():
         for index, i_elem in enumerate(sorted_list):
             if i_value > i_elem[1]:
                 sorted_list.insert(index + 1, (i_key, i_value))
-                sorted_list[index], sorted_list[index + 1] = sorted_list[index + 1], sorted_list[index]
+                sorted_list[index], sorted_list[index + 1] = (
+                    sorted_list[index + 1],
+                    sorted_list[index],
+                )
                 break
             elif i_value == i_elem[1]:
                 if alphabet_dict[i_key] < alphabet_dict[i_elem[0]]:
                     sorted_list.insert(index + 1, (i_key, i_value))
-                    sorted_list[index], sorted_list[index + 1] = sorted_list[index + 1], sorted_list[index]
+                    sorted_list[index], sorted_list[index + 1] = (
+                        sorted_list[index + 1],
+                        sorted_list[index],
+                    )
                     break
         else:
             sorted_list.append((i_key, i_value))
 
-result = open('analysis.txt', 'w')
+result = open("analysis.txt", "w")
 
 for i_letter in sorted_list:
-    result.write(f'{i_letter[0]} {round(i_letter[1] / letter_counter, 3)}\n')
+    result.write(f"{i_letter[0]} {round(i_letter[1] / letter_counter, 3)}\n")
 result.close()

42bfb35ee33e64aa06dc1c70d819dea0340e942b
DKrivov <d.krivov@kitcup.ru>
2024-02-14 09:46:55 +0300

    0214

--- Tutorials for Python/Basic_Skillbox/Module23/01_names_2/main.py
+++ Tutorials for Python/Basic_Skillbox/Module23/01_names_2/main.py
@@ -4,18 +4,20 @@
 common_long = 0
 line = 0
 
-with open('people.txt', 'r', encoding='utf8') as people_file, open('errors.log', 'w', encoding='utf8') as error_file:
+with open("people.txt", encoding="utf8") as people_file, open(
+    "errors.log", "w", encoding="utf8"
+) as error_file:
     for man in people_file:
         line += 1
         try:
             if len(man) - 1 < 3:
-                text_error = f'Ошибка: менее трёх символов в строке {line}'
+                text_error = f"Ошибка: менее трёх символов в строке {line}"
                 raise BaseException(text_error)
             common_long += len(man) - 1
         except BaseException:
             print(text_error)
             error_file.write(f"BaseException({text_error})")
     else:
-        print('Общее количество символов: ', common_long)
+        print("Общее количество символов: ", common_long)
 
 # зачтено

96d6761252646a8b3eb1eb4d288ad88f24043cbf
DKrivov <d.krivov@kitcup.ru>
2024-02-17 10:50:17 +0300

    0217

--- Tutorials for Python/Basic_Skillbox/Module23/04_chat/main.py
+++ Tutorials for Python/Basic_Skillbox/Module23/04_chat/main.py
@@ -3,32 +3,34 @@
 
 def file_print() -> None:
     try:
-        with open('messages.txt', 'r', encoding='utf-8') as messages_file:
+        with open("messages.txt", encoding="utf-8") as messages_file:
             for i_massage in messages_file:
-                print(i_massage, end='')
+                print(i_massage, end="")
             else:
                 print()
     except FileNotFoundError:
-        print('В чате нет ни одного сообщения\n')
+        print("В чате нет ни одного сообщения\n")
 
 
 def new_massage(name: str, message: str) -> None:
-    with open('messages.txt', 'a', encoding='utf-8') as messages_file:
-        messages_file.write('{0}: {1}\n'.format(name, message))
+    with open("messages.txt", "a", encoding="utf-8") as messages_file:
+        messages_file.write(f"{name}: {message}\n")
 
 
-user_name = input('Представьтесь: ')
+user_name = input("Представьтесь: ")
 while True:
-    action = input('1. Посмотреть текущий текст чата.\n'
-                   '2. Отправить сообщение (затем вводит сообщение).\n'
-                   '3. Выход\n')
-    if action == '1':
+    action = input(
+        "1. Посмотреть текущий текст чата.\n"
+        "2. Отправить сообщение (затем вводит сообщение).\n"
+        "3. Выход\n",
+    )
+    if action == "1":
         file_print()
-    elif action == '2':
-        new_massage(user_name, input('Введите текст сообщения: '))
-    elif action == '3':
+    elif action == "2":
+        new_massage(user_name, input("Введите текст сообщения: "))
+    elif action == "3":
         break
     else:
-        print('Выбрано некорректное действие.')
+        print("Выбрано некорректное действие.")
 
 # зачтено

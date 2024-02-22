650a0d577540120af708e69c1feac4191c46879b
DKrivov <d.krivov@kitcup.ru>
2024-02-22 10:52:25 +0300

    0222

--- Tutorials for Python/Basic_Skillbox/Module24/05_cohabitation/main.py
+++ Tutorials for Python/Basic_Skillbox/Module24/05_cohabitation/main.py
@@ -1,27 +1,37 @@
 # TODO здесь писать код
 
-class House:
 
+class House:
     def __init__(self, food: int = 50, money: int = 0):
         self.refrigerator = food
         self.nightstand = money
 
     def info(self):
-        print('Дом, адрес - не указан, холодильник полон на {food}, '
-              'уровень денег - {money}'.format(
-            food = self.refrigerator, money = self.nightstand))
+        print(
+            "Дом, адрес - не указан, холодильник полон на {food}, "
+            "уровень денег - {money}".format(
+                food=self.refrigerator,
+                money=self.nightstand,
+            ),
+        )
+
 
 class Human:
-
     def __init__(self, name: str, home: House, satiety: int = 50):
         self.name = name
         self.satiety = satiety
         self.home = home
 
     def info(self):
-        print('Меня зовут {h_name}, сыт(а) я на {h_satiety}, '
-              'живу в {house}'.format(
-            h_name = self.name, h_satiety = self.satiety, house = self.home))
+        print(
+            "Меня зовут {h_name}, сыт(а) я на {h_satiety}, "
+            "живу в {house}".format(
+                h_name=self.name,
+                h_satiety=self.satiety,
+                house=self.home,
+            ),
+        )
+
     def eat(self):
         # print('Можно и поесть')
         if self.home.refrigerator >= 5:
@@ -34,14 +44,14 @@ class Human:
         # print('На любимую работу')
         self.satiety -= 10
         if self.satiety <= 0:
-            raise BaseException('Сытость упала до 0 - человек умер :(')
+            raise BaseException("Сытость упала до 0 - человек умер :(")
         self.home.nightstand += 10
 
     def play(self):
         # print('Меня нет, я играть, буду когда вернусь')
         self.satiety -= 10
         if self.satiety <= 0:
-            raise BaseException('Сытость упала до 0 - человек умер :(')
+            raise BaseException("Сытость упала до 0 - человек умер :(")
 
     def restock(self):
         # print('Опят/ь в Шестёрочку переться.')
@@ -51,14 +61,15 @@ class Human:
             self.home.refrigerator += 5
             print(self.home.refrigerator)
         else:
-            print('Денег недостаточно :(')
+            print("Денег недостаточно :(")
+
 
 from random import randint
 
 
 house = House()
-man_1 = Human('Василий', house)
-woman_1 = Human('Лера', house)
+man_1 = Human("Василий", house)
+woman_1 = Human("Лера", house)
 
 house.info()
 man_1.info()
@@ -95,7 +106,7 @@ for _ in range(1000):
         woman_1.play()
 
 else:
-    print('\nПрошёл год\n')
+    print("\nПрошёл год\n")
     house.info()
     man_1.info()
     woman_1.info()

ab8b3877676ab87130b2bcf15b12181c9f5f6d5b
DKrivov <d.krivov@kitcup.ru>
2024-02-23 16:21:34 +0300

    0224

--- Tutorials for Python/Basic_Skillbox/Module25/03_my_dict/main.py
+++ Tutorials for Python/Basic_Skillbox/Module25/03_my_dict/main.py
@@ -1,7 +1,8 @@
 # TODO здесь писать код
 
+
 class MyDict(dict):
-    """ Класс словаря Родитель dict. """
+    """Класс словаря Родитель dict."""
 
     def __init__(self, seq=None, **kwargs):
         super().__init__(**kwargs)
@@ -11,11 +12,15 @@ class MyDict(dict):
         Переопределение функции get родительского словаря.
         Возвращает 0 вместо None если элемента нет в словаре.
         """
-        return super().get(*args, **kwargs) if not super().get(*args, **kwargs) is None else 0
+        return (
+            super().get(*args, **kwargs)
+            if not super().get(*args, **kwargs) is None
+            else 0
+        )
 
 
 my_dict = MyDict()
-my_dict[1] = '1'
+my_dict[1] = "1"
 
 print(my_dict.get(2))
 

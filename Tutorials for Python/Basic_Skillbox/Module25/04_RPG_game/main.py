25b8007f26620924a3723b8c89e157807004eff7
DKrivov <d.krivov@kitcup.ru>
2024-02-24 07:43:59 +0300

    0224

--- Tutorials for Python/Basic_Skillbox/Module25/04_RPG_game/main.py
+++ Tutorials for Python/Basic_Skillbox/Module25/04_RPG_game/main.py
@@ -23,7 +23,10 @@ def one_year_of_war():
     # Если монстры умирают - они пропадают из списка
     # Если умирают герои - цикл завершается - битва считается проигранной (возвращается 0)
     # Если герои выживают - битва считается выигранной (возвращается 1)
-    if sum([isinstance(hero, (MonsterHunter, MonsterBerserk)) for hero in good_team]) > 1:
+    if (
+        sum([isinstance(hero, (MonsterHunter, MonsterBerserk)) for hero in good_team])
+        > 1
+    ):
         print("В команде героев может быть только 1 монстр!")
         return 0
 
@@ -38,22 +41,22 @@ def one_year_of_war():
         # В циклах у героев и монстров вызывается метод make_a_move, который должен выбирать и совершать одно действие
         # Для наглядности вы можете добавлять в каждое действие принты с подробностями (чтобы знать кто когда и что совершает)
         # При помощи этой информации вы сможете искать проблемы и ошибки в вашем коде и в конечном итоге это поможет вам улучшить стратегию
-        print("\nКоманда добра:\n" + '-' * 50)
+        print("\nКоманда добра:\n" + "-" * 50)
         for hero in good_team:
             hero.make_a_move(good_team, evil_team)
 
-        print("\nКоманда зла:\n" + '-' * 50)
+        print("\nКоманда зла:\n" + "-" * 50)
         for mob in evil_team:
             mob.make_a_move(evil_team, good_team)
 
         print(f"Итоги дня сражений №{day}")
 
         # В итогах дня у каждого героя и каждого монстра вызывается метод __str__ который должен описывать их текущее состояние
-        print("\nКоманда добра:\n" + '-' * 50)
+        print("\nКоманда добра:\n" + "-" * 50)
         for hero in good_team:
             print(hero)
 
-        print("\nКоманда зла:\n" + '-' * 50)
+        print("\nКоманда зла:\n" + "-" * 50)
         for mob in evil_team:
             print(mob)
 
@@ -61,8 +64,10 @@ def one_year_of_war():
         evil_team = [mob for mob in evil_team if mob.is_alive()]
         # Новые монстры в чётные дни добавляются в список (но их не может быть больше 4)
         if day % 2 == 0 and len(evil_team) < 4:
-            newborn_evils = [MonsterBerserk("Берсерк " + random.choice(evil_names)),
-                             MonsterHunter("Рейнджер " + random.choice(evil_names))]
+            newborn_evils = [
+                MonsterBerserk("Берсерк " + random.choice(evil_names)),
+                MonsterHunter("Рейнджер " + random.choice(evil_names)),
+            ]
             evil_team.append(random.choice(newborn_evils))
 
         if any([not hero.is_alive() for hero in good_team]):

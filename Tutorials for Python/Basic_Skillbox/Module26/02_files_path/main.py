3c53fc50bc694eaca884a528fd4a601b40c4cd56
DKrivov <d.krivov@kitcup.ru>
2024-02-27 00:01:23 +0300

    0227

--- Tutorials for Python/Basic_Skillbox/Module26/02_files_path/main.py
+++ Tutorials for Python/Basic_Skillbox/Module26/02_files_path/main.py
@@ -5,13 +5,13 @@ from typing import Optional
 
 
 def gen_files_path(dir_name: str, path: str = os.path.abspath(os.sep)) -> Optional[str]:
-    result_paths = ''
+    result_paths = ""
     for i_elem in os.listdir(path):
         if i_elem == dir_name:
             path_gen = os.walk(os.path.join(path, i_elem))
             for i_path in path_gen:
                 for i_file in i_path[2]:
-                    result_paths += '{}\n'.format(os.path.join(i_path[0], i_file))
+                    result_paths += f"{os.path.join(i_path[0], i_file)}\n"
             else:
                 return result_paths
         elif os.path.isdir(os.path.join(path, i_elem)):
@@ -22,6 +22,6 @@ def gen_files_path(dir_name: str, path: str = os.path.abspath(os.sep)) -> Option
         return None
 
 
-print(gen_files_path('Module19', path=os.path.abspath("../../../Python_Basic")))
+print(gen_files_path("Module19", path=os.path.abspath("../../../Python_Basic")))
 
 # зачтено

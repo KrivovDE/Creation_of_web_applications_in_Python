871d96afd56852e23e38b099cc30b36674ec86b4
DKrivov <d.krivov@kitcup.ru>
2024-02-28 09:03:48 +0300

    0228

--- Tutorials for Python/Basic_Skillbox/Module26/03_str_count/main.py
+++ Tutorials for Python/Basic_Skillbox/Module26/03_str_count/main.py
@@ -4,11 +4,11 @@ import os
 
 def str_counter_gen():
     for i_file in os.listdir():
-        if '.py' in i_file:
+        if ".py" in i_file:
             counter = 0
-            with open(i_file, 'r', encoding='utf-8') as file:
+            with open(i_file, encoding="utf-8") as file:
                 for i_str in file:
-                    if not (len(i_str) < 2 or '"""' in i_str or '#' in i_str):
+                    if not (len(i_str) < 2 or '"""' in i_str or "#" in i_str):
                         counter += 1
                 else:
                     yield counter

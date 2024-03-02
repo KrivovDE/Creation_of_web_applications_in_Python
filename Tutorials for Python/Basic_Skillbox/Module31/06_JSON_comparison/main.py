f49a6769c0e9a3d00d1f7653ba1f1c21b1b05c1b
DKrivov <d.krivov@kitcup.ru>
2024-03-02 12:19:46 +0300

    0302

--- Tutorials for Python/Basic_Skillbox/Module31/06_JSON_comparison/main.py
+++ Tutorials for Python/Basic_Skillbox/Module31/06_JSON_comparison/main.py
@@ -5,15 +5,14 @@ import json
 diff_list = ["services", "staff", "datetime"]
 result_dict = dict()
 
-with open('json_old.json', 'r') as old_file, \
-        open('json_new.json', 'r') as new_file:
-    old_data = json.load(old_file)['data']
-    new_data = json.loads(new_file.read())['data']
+with open("json_old.json") as old_file, open("json_new.json") as new_file:
+    old_data = json.load(old_file)["data"]
+    new_data = json.loads(new_file.read())["data"]
 
     for i_parm in diff_list:
         if new_data[i_parm] != old_data[i_parm]:
             result_dict[i_parm] = [new_data[i_parm], old_data[i_parm]]
 
-    with open('result.json', 'w') as result_file:
+    with open("result.json", "w") as result_file:
         result_file.write(json.dumps(result_dict, indent=4))
     print(result_dict)

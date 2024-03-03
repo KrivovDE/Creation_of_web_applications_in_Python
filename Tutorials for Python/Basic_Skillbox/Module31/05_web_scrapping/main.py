0f176ebb77d3f561a606bcbd1c0006e332f92a2d
DKrivov <d.krivov@kitcup.ru>
2024-03-03 13:57:31 +0300

    0303

--- Tutorials for Python/Basic_Skillbox/Module31/05_web_scrapping/main.py
+++ Tutorials for Python/Basic_Skillbox/Module31/05_web_scrapping/main.py
@@ -2,7 +2,7 @@
 import re
 
 # В данном случае запрос request.get заменен на загрзку сайта из файла html
-with open('examples.html', 'r') as f:
+with open("examples.html") as f:
     text = f.read()
-# По итогу вы так же получаете код сайта в виде одной строки
-    print(re.findall(r'(?<=<h3>).+(?=</h3>)', text))
+    # По итогу вы так же получаете код сайта в виде одной строки
+    print(re.findall(r"(?<=<h3>).+(?=</h3>)", text))

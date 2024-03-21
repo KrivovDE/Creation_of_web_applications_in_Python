015ee65092c57f5a9f519e21e3a4d648328c8913
Dmitrii Krivov <d.krivov@kitcup.ru>
2024-03-21 10:09:18 +0300

    0321

--- Tutorials for Python/Basic_Skillbox/Module30/02_zip_again/main.py
+++ Tutorials for Python/Basic_Skillbox/Module30/02_zip_again/main.py
@@ -2,8 +2,8 @@
 
 from typing import List
 
-letters: List[str] = ['a', 'b', 'c', 'd', 'e']
-numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+letters: list[str] = ["a", "b", "c", "d", "e"]
+numbers: list[int] = [1, 2, 3, 4, 5, 6, 7, 8]
 
 results = list(map(lambda x, y: (x, y), letters, numbers))
 

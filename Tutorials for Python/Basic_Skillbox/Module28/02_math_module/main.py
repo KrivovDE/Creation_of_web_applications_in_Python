69e3725009bce24181fb519b62699f41ece97566
Dmitrii Krivov <d.krivov@kitcup.ru>
2024-08-26 09:56:55 +0300

    0826

--- Tutorials for Python/Basic_Skillbox/Module28/02_math_module/main.py
+++ Tutorials for Python/Basic_Skillbox/Module28/02_math_module/main.py
@@ -2,6 +2,7 @@
 
 from abc import ABC
 
+
 class MyMath(ABC):
 
     """Абстрактный класс математических операций"""
@@ -11,22 +12,22 @@ class MyMath(ABC):
     @classmethod
     def circle_len(cls, radius: float) -> float:
         """Возвращает длину окружности исходя из входного радиуса"""
-        return 2 * cls._Pi*radius
+        return 2 * cls._Pi * radius
 
     @classmethod
     def circle_sq(cls, radius: float) -> float:
         """Возвращает площадь окружности исходя из входного радиуса"""
-        return 2 * cls._Pi * radius ** 2
+        return 2 * cls._Pi * radius**2
 
     @classmethod
     def cub_vol(cls, fin_length: float) -> float:
         """Возвращает объём куда исходя из длинны ребра"""
-        return fin_length ** 3
+        return fin_length**3
 
     @classmethod
     def surface_area_sphere(cls, radius: float) -> float:
         """Возвращает площадь поверхности сферы исходя из входного радиуса"""
-        return 2 * cls._Pi*radius ** 2
+        return 2 * cls._Pi * radius**2
 
 
 res_1 = MyMath.circle_len(radius=5)

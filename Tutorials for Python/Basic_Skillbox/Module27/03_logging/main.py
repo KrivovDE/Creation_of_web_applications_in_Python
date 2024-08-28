e7d541a4feca4be60c6319808cfa92c252bd35e8
Dmitrii Krivov <d.krivov@kitcup.ru>
2024-08-28 10:10:06 +0300

    0828

--- Tutorials for Python/Basic_Skillbox/Module27/03_logging/main.py
+++ Tutorials for Python/Basic_Skillbox/Module27/03_logging/main.py
@@ -11,13 +11,17 @@ def logging(func: Callable) -> Callable:
         try:
             return func(*args, **kwargs)
         except TypeError:
-            with open('function_errors.log', 'a', encoding='utf-8') as log_file:
-                log_file.write('{time}: Функция - {func_name}, '
-                               'завершилось ошибкой - {error} \n\t'
-                               'Документация: {doc}\n'.format(
-                    func_name=func.__name__, time=datetime.now(),
-                    error=TypeError, doc=func.__doc__
-                ))
+            with open("function_errors.log", "a", encoding="utf-8") as log_file:
+                log_file.write(
+                    "{time}: Функция - {func_name}, "
+                    "завершилось ошибкой - {error} \n\t"
+                    "Документация: {doc}\n".format(
+                        func_name=func.__name__,
+                        time=datetime.now(),
+                        error=TypeError,
+                        doc=func.__doc__,
+                    ),
+                )
 
     return wrapped_func
 
@@ -25,25 +29,25 @@ def logging(func: Callable) -> Callable:
 @logging
 def say_hi_1(name: str) -> None:
     """Функция приветствует пользователя"""
-    print('Привет, {name}'.format(name=name))
+    print(f"Привет, {name}")
 
 
 @logging
 def say_hi_2(name: str) -> None:
     """Функция приветствует пользователя и делит стоку на число"""
-    print('Привет, {name}'.format(name=name))
-    a = 'b0' / 2
+    print(f"Привет, {name}")
+    "b0" / 2
 
 
 @logging
 def say_hi_3(name: str) -> None:
     """Функция приветствует пользователя и округляет строку"""
-    b = round('b0', 2)
-    print('Привет, {name}'.format(name=name))
+    round("b0", 2)
+    print(f"Привет, {name}")
 
 
-say_hi_1('Valyera')
-say_hi_2('Ivan')
-say_hi_3('Kostya')
+say_hi_1("Valyera")
+say_hi_2("Ivan")
+say_hi_3("Kostya")
 
 # зачтено

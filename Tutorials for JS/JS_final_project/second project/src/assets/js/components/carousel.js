8e5ea81fb1d2efae47606d5e915c91cfdb6ef4a3
Dmitrii Krivov <d.krivov@kitcup.ru>
2024-10-18 07:30:04 +0300

    1018

--- Tutorials for JS/JS_final_project/second project/src/assets/js/components/carousel.js
+++ Tutorials for JS/JS_final_project/second project/src/assets/js/components/carousel.js
@@ -1,18 +1,17 @@
 //карусель
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        stagePadding: 90,
-        loop: true,
-        nav: true,
-        navText: [
-            '<img src="./assets/img/svg/left.svg" alt="service" class="owl-carousel-back">',
-            '<img src="./assets/img/svg/right.svg" alt="service" class="owl-carousel-next">'
-        ],
-        dots: true,
-        dotsContainer: '.dots'
-
-    });
-    $('.owl-stage').css({'padding-left': '0'})
-    new WOW().init();
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    stagePadding: 90,
+    loop: true,
+    nav: true,
+    navText: [
+      '<img src="./assets/img/svg/left.svg" alt="service" class="owl-carousel-back">',
+      '<img src="./assets/img/svg/right.svg" alt="service" class="owl-carousel-next">',
+    ],
+    dots: true,
+    dotsContainer: ".dots",
+  });
+  $(".owl-stage").css({ "padding-left": "0" });
+  new WOW().init();
 });
